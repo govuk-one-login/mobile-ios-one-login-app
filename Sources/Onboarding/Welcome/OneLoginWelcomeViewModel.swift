f701dc8ce1b8cd222c88f126b08eed93d2cbb402
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2023-10-05 15:44:17 +0100

    feat: one login welcome screen working

--- Sources/Onboarding/Welcome/OneLoginWelcomeViewModel.swift
+++ Sources/Onboarding/Welcome/OneLoginWelcomeViewModel.swift
@@ -6,9 +6,9 @@ import UIKit
 
 struct OneLoginWelcomeViewModel: WelcomeViewModel {
     var image: UIImage = UIImage(named: "badge") ?? UIImage()
-    var title: GDSLocalisedString = ""
-    var body: GDSLocalisedString = ""
-    var welcomeButtonViewModel: ButtonViewModel = AnalyticsButtonViewModel(titleKey: "",
+    var title: GDSLocalisedString = "GOV.UK One Login"
+    var body: GDSLocalisedString = "This is a short description of the GOV.UK One Login application as a demonstration of the screen"
+    var welcomeButtonViewModel: ButtonViewModel = AnalyticsButtonViewModel(titleKey: "Continue",
                                                                            analyticsService: GAnalytics(),
                                                                            action: { })
     
