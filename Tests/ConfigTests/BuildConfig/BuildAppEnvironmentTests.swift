677197844e8aae3959ea06e2ad0aafbf39773ef8
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2023-11-06 11:45:42 +0000

    chore: adding testing action to merged workflow

--- Tests/ConfigTests/BuildConfig/BuildAppEnvironmentTests.swift
+++ Tests/ConfigTests/BuildConfig/BuildAppEnvironmentTests.swift
@@ -4,7 +4,7 @@ import XCTest
 final class BuildAppEnvironmentTests: XCTestCase {
     func test_defaultEnvironment_retrieveFromPlist() throws {
         let sut = AppEnvironment.self
-        XCTAssertEqual(sut.oneLoginAuthorize, URL(string: "https://auth-stub.mobile.dev.account.gov.uk/authorize"))
+        XCTAssertEqual(sut.oneLoginAuthorize, URL(string: "https://auth-stub.mobile.build.account.gov.uk/authorize"))
         XCTAssertEqual(sut.oneLoginToken, URL(string: "https://test.com/test"))
         XCTAssertEqual(sut.oneLoginClientID, "TEST_CLIENT_ID")
         XCTAssertEqual(sut.oneLoginRedirect, "https://mobile.build.account.gov.uk/redirect")
