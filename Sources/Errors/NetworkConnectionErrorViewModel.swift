1bacbfc16c098ec9447fdcc919d48f096c3923c9
Shukri Ahmed <shukri.ahmed@digital.cabinet-office.gov.uk>
2024-01-03 12:01:50 +0000

    chore: changed variables to constants

--- Sources/Errors/NetworkConnectionErrorViewModel.swift
+++ Sources/Errors/NetworkConnectionErrorViewModel.swift
@@ -3,16 +3,16 @@ import GDSCommon
 import Logging
 
 struct NetworkConnectionErrorViewModel: GDSErrorViewModel, BaseViewModel {
-    var image: String = "exclamationmark.circle"
+    let image: String = "exclamationmark.circle"
     // TODO: DCMAW-7083: String keys for localisation needed
-    var title: GDSLocalisedString = "You appear to be offline"
-    var body: GDSLocalisedString = "GOV.UK One Login is not avaliable offline. \nReconnect to the internet and try again."
-    var primaryButtonViewModel: ButtonViewModel
-    var secondaryButtonViewModel: ButtonViewModel? = nil
+    let title: GDSLocalisedString = "You appear to be offline"
+    let body: GDSLocalisedString = "GOV.UK One Login is not avaliable offline. \nReconnect to the internet and try again."
+    let primaryButtonViewModel: ButtonViewModel
+    let secondaryButtonViewModel: ButtonViewModel? = nil
     let analyticsService: AnalyticsService
     
-    var rightBarButtonTitle: GDSLocalisedString? = nil
-    var backButtonIsHidden: Bool = true
+    let rightBarButtonTitle: GDSLocalisedString? = nil
+    let backButtonIsHidden: Bool = true
     
     init(analyticsService: AnalyticsService, action: @escaping () -> Void) {
         self.analyticsService = analyticsService
