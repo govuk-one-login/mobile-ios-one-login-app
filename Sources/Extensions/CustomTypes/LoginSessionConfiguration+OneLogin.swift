73e2d5aae70c618611581fbf96a236c404cb77b0
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2024-01-15 13:06:59 +0000

    refactor: using custom scope to pass sts

--- Sources/Extensions/CustomTypes/LoginSessionConfiguration+OneLogin.swift
+++ Sources/Extensions/CustomTypes/LoginSessionConfiguration+OneLogin.swift
@@ -5,7 +5,7 @@ extension LoginSessionConfiguration {
         let env = AppEnvironment.self
         return .init(authorizationEndpoint: env.callingSTSEnabled ? env.stsLoginAuthorize : env.oneLoginAuthorize,
                      tokenEndpoint: env.oneLoginToken,
-                     scopes: env.callingSTSEnabled ? [.sts] : [.openid],
+                     scopes: env.callingSTSEnabled ? [.custom("sts")] : [.openid],
                      clientID: env.callingSTSEnabled ? env.stsClientID : env.oneLoginClientID,
                      redirectURI: env.oneLoginRedirect)
     }
