58419ff5f123815654000ba1edbfaaa43cd2d386
mcgarj <joe.mcgarry@digital.cabinet-office.gov.uk>
2024-02-07 15:34:40 +0000

    chore: post-rebase cleanup

--- Sources/Utilities/LAContexting.swift
+++ Sources/Utilities/LAContexting.swift
@@ -2,6 +2,7 @@ import LocalAuthentication
 
 protocol LAContexting {
     func canEvaluatePolicy(_ policy: LAPolicy, error: NSErrorPointer) -> Bool
+    var biometryType: LABiometryType { get }
 }
 
 extension LAContext: LAContexting { }
