2e007a8c4c7990f975d591beec39daeb51d329dc
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2024-02-20 15:06:03 +0000

    test: adding OnboardingCoordinator test cases for failing or erroring biometrics

--- Tests/UnitTests/Mocks/Sessions+Services/MockLAContext.swift
+++ Tests/UnitTests/Mocks/Sessions+Services/MockLAContext.swift
@@ -4,18 +4,23 @@ import LocalAuthentication
 final class MockLAContext: LAContexting {
     var biometryType: LABiometryType = .touchID
     
-    var returnedFromEvaluatePolicyForBiometrics: Bool = false
-    var returnedFromEvaluatePolicyForAuthentication: Bool = false
+    var returnedFromCanEvaluatePolicyForBiometrics: Bool = false
+    var returnedFromCanEvaluatePolicyForAuthentication: Bool = false
+    var errorFromEvaluatePolicy: Error?
+    var returnedFromEvaluatePolicy: Bool = true
     
     func canEvaluatePolicy(_ policy: LAPolicy, error: NSErrorPointer) -> Bool {
         if policy == .deviceOwnerAuthenticationWithBiometrics {
-            return returnedFromEvaluatePolicyForBiometrics
+            return returnedFromCanEvaluatePolicyForBiometrics
         } else {
-            return returnedFromEvaluatePolicyForAuthentication
+            return returnedFromCanEvaluatePolicyForAuthentication
         }
     }
     
     func evaluatePolicy(_ policy: LAPolicy, localizedReason: String) async throws -> Bool {
-        <#code#>
+        if let errorFromEvaluatePolicy {
+            throw errorFromEvaluatePolicy
+        }
+        return returnedFromEvaluatePolicy
     }
 }
