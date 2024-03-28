0a61a9722607cd0cf33130f524daf497bf9fd1f5
Joshua Dubey <josh.dubey@digital.cabinet-office.gov.uk>
2024-03-28 13:19:21 +0000

    feat:  add additional parameters for new GA4 schema

--- Sources/Utilities/Analytics/AnalyticsCenter.swift
+++ Sources/Utilities/Analytics/AnalyticsCenter.swift
@@ -8,5 +8,15 @@ final class AnalyticsCenter: AnalyticsCentral {
          analyticsPreferenceStore: AnalyticsPreferenceStore) {
         self.analyticsService = analyticsService
         self.analyticsPreferenceStore = analyticsPreferenceStore
+        setAdditionalParameters()
+    }
+    
+    private func setAdditionalParameters() {
+        analyticsService.additionalParameters = [
+            "primary_publishing_organisation": "government digital service - digital identity",
+            "organisation": "<OT1056>",
+            "taxonomy_level1": "one login mobile application",
+            "taxonomy_level2": "login"
+        ]
     }
 }
