88678fe338bd4421b6f46bd024ffc3ac30fdf2d3
mcgarj <117986969+mcgarj@users.noreply.github.com>
2024-04-10 09:41:48 +0100

    feat: initial implementation of GA4 for screen (#95)

--- Sources/Analytics/Onboarding/InformationAnalytics.swift
+++ Sources/Analytics/Onboarding/InformationAnalytics.swift
@@ -4,3 +4,7 @@ import Logging
 enum InformationAnalyticsScreen: String, ScreenType {
     case passcode = "passcodeInformationScreen"
 }
+
+enum InformationAnalyticsScreenID: String {
+    case passcodeInfoScreen = "7412b60c-8f06-4d6c-9ecc-57769516ebac"
+}
--- Tests/UnitTests/Login/ViewModels/PasscodeInformationViewModelTests.swift
+++ Tests/UnitTests/Login/ViewModels/PasscodeInformationViewModelTests.swift
@@ -38,19 +38,31 @@ extension PasscodeInformationViewModelTests {
         sut.primaryButtonViewModel.action()
         XCTAssertTrue(didCallButtonAction)
         XCTAssertEqual(mockAnalyticsService.eventsLogged.count, 1)
-        let event = ButtonEvent(textKey: "app_continueButton")
+        let event = LinkEvent(textKey: "app_continueButton",
+                              linkDomain: AppEnvironment.oneLoginBaseURL,
+                              external: .false)
         XCTAssertEqual(mockAnalyticsService.eventsLogged, [event.name.name])
-        XCTAssertEqual(mockAnalyticsService.eventsParamsLogged["text"], event.parameters["text"])
-        XCTAssertEqual(mockAnalyticsService.eventsParamsLogged["type"], event.parameters["type"])
+        XCTAssertEqual(mockAnalyticsService.eventsParamsLogged["text"],
+                       event.parameters["text"])
+        XCTAssertEqual(mockAnalyticsService.eventsParamsLogged["type"],
+                       event.parameters["type"])
+        XCTAssertEqual(mockAnalyticsService.eventsParamsLogged["link_domain"],
+                       event.parameters["link_domain"])
+        XCTAssertEqual(mockAnalyticsService.eventsParamsLogged["external"],
+                       event.parameters["external"])
     }
     
     func test_didAppear() throws {
         XCTAssertEqual(mockAnalyticsService.screensVisited.count, 0)
         sut.didAppear()
         XCTAssertEqual(mockAnalyticsService.screensVisited.count, 1)
-        let screen = ScreenView(screen: InformationAnalyticsScreen.passcode,
+        let screen = ScreenView(id: InformationAnalyticsScreenID.passcodeInfoScreen.rawValue,
+                                screen: InformationAnalyticsScreen.passcode,
                                 titleKey: "app_noPasscodeSetupTitle")
         XCTAssertEqual(mockAnalyticsService.screensVisited, [screen.name])
-        XCTAssertEqual(mockAnalyticsService.screenParamsLogged["title"], screen.parameters["title"])
+        XCTAssertEqual(mockAnalyticsService.screenParamsLogged["title"],
+                       screen.parameters["title"])
+        XCTAssertEqual(mockAnalyticsService.screenParamsLogged["screen_id"],
+                       screen.parameters["screen_id"])
     }
 }
