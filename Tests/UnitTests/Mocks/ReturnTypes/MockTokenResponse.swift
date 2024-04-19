5e2606d2c1bad124e51c8cdec654f22d1554a9db
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2024-04-19 17:04:45 +0100

    refactor: minor changes

--- Tests/UnitTests/Mocks/ReturnTypes/MockTokenResponse.swift
+++ Tests/UnitTests/Mocks/ReturnTypes/MockTokenResponse.swift
@@ -7,20 +7,19 @@ class MockTokenResponse {
     }
     
     func getJSONData(outdated: Bool = false) throws -> TokenResponse {
-        let bundleDoingTest = Bundle(for: type(of: self))
-        guard let jsonPath = bundleDoingTest.path(forResource: "\(outdated ? "Outdated" : "")TokenResponse", ofType: "json"),
+        let bundleForTest = Bundle(for: type(of: self))
+        guard let jsonPath = bundleForTest.path(forResource: (outdated ? "OutdatedTokenResponse" : "TokenResponse"), ofType: "json"),
               let jsonData = FileManager.default.contents(atPath: jsonPath) else {
             throw DecodeError.invalid
         }
         
         let decoder = JSONDecoder()
         decoder.keyDecodingStrategy = .convertFromSnakeCase
-        decoder.dateDecodingStrategy = .custom { decoder in
-            let container = try decoder.singleValueContainer()
-            let dateInt = try container.decode(Double.self)
-            return Date(timeIntervalSinceNow: dateInt)
+        decoder.dateDecodingStrategy = .custom {
+            let container = try $0.singleValueContainer()
+            let dateDouble = try container.decode(Double.self)
+            return Date(timeIntervalSinceNow: dateDouble)
         }
-        let tokenResponse = try decoder.decode(TokenResponse.self, from: jsonData)
-        return tokenResponse
+        return try decoder.decode(TokenResponse.self, from: jsonData)
     }
 }
