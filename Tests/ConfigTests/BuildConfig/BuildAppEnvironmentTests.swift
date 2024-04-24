83005c0c1d3e988d6d945557248bc75366075c45
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2024-04-24 16:32:28 +0100

    fix: correcting build configuration test

--- Tests/ConfigTests/BuildConfig/BuildAppEnvironmentTests.swift
+++ Tests/ConfigTests/BuildConfig/BuildAppEnvironmentTests.swift
@@ -7,7 +7,7 @@ final class BuildAppEnvironmentTests: XCTestCase {
         XCTAssertEqual(sut.oneLoginAuthorize, URL(string: "https://auth-stub.mobile.build.account.gov.uk/authorize"))
         XCTAssertEqual(sut.stsLoginAuthorize, URL(string: "https://token.build.account.gov.uk/authorize"))
         XCTAssertEqual(sut.oneLoginToken, URL(string: "https://mobile.build.account.gov.uk/token"))
-        XCTAssertEqual(sut.stsLoginToken, URL(string: "https://token.staging.account.gov.uk/token"))
+        XCTAssertEqual(sut.stsLoginToken, URL(string: "https://token.build.account.gov.uk/token"))
         XCTAssertEqual(sut.privacyPolicyURL, URL(string: "https://signin.account.gov.uk/privacy-notice?lng=en"))
         XCTAssertEqual(sut.oneLoginClientID, "TEST_CLIENT_ID")
         XCTAssertEqual(sut.stsClientID, "bYrcuRVvnylvEgYSSbBjwXzHrwJ")
