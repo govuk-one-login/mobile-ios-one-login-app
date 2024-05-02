692642e140d482f98c66a0c22e0a7c5f9bc5da83
Joshua Dubey <josh.dubey@digital.cabinet-office.gov.uk>
2024-05-02 09:03:20 +0100

    chore:  Add helper protocol and reorganize file.

---
+++ Sources/Utilities/Factories/TabbedViewSectionFactory.swift
@@ -0,0 +1,61 @@
+import Foundation
+import GDSCommon
+
+struct TabbedViewSectionFactory {
+    
+    @MainActor
+    static func homeSections(coordinator: HomeCoordinator) -> [TabbedViewSectionModel] {
+        #if DEBUG
+        let homeSection = createSection(header: "Developer Menu",
+                                  footer: nil,
+                                  cellModels: [.init(cellTitle: "Developer Menu") {
+            coordinator.showDeveloperMenu()
+        }])
+        #else
+        let homeSection = TabbedViewSectionModel()
+        #endif
+        return [homeSection]
+    }
+
+    static func profileSections(urlOpener: URLOpener) -> [TabbedViewSectionModel] {
+        let manageDetailsSection = createSection(header: "app_profileSubtitle1",
+                                                                          footer: "app_manageSignInDetailsFootnote",
+                                                                          cellModels: [.init(cellTitle: "app_manageSignInDetailsLink",
+                                                                                             accessoryView: "arrow.up.right") {
+            urlOpener.open(url: AppEnvironment.manageAccountURL)
+        }])
+                
+        let legalSection = createSection(header: "app_profileSubtitle2",
+                                                                  footer: nil,
+                                                                  cellModels: [.init(cellTitle: "app_privacyNoticeLink2",
+                                                                                     accessoryView: "arrow.up.right") {
+            urlOpener.open(url: AppEnvironment.privacyPolicyURL)
+        }])
+        
+        let helpSection = createSection(header: "app_profileSubtitle3",
+                                                                 footer: nil,
+                                                                 cellModels: [.init(cellTitle: "app_reportAProblemGiveFeedbackLink",
+                                                                                    accessoryView: "arrow.up.right"),
+                                                                              .init(cellTitle: "app_appGuidanceLink",
+                                                                                    accessoryView: "arrow.up.right")])
+        let signoutSection = createSection(header: nil,
+                                                                    footer: nil,
+                                                                    cellModels: [.init(cellTitle: "app_signOutButton",
+                                                                                       textColor: .gdsRed)])
+        
+        return [manageDetailsSection,
+                legalSection,
+                helpSection,
+                signoutSection]
+    }
+    
+    static func createSection(header: GDSLocalisedString?,
+                              footer: GDSLocalisedString?,
+                              cellModels: [TabbedViewCellModel]) -> TabbedViewSectionModel {
+        
+        return TabbedViewSectionModel(sectionTitle: header,
+                                      sectionFooter: footer,
+                                      tabModels: cellModels)
+        
+    }
+}
