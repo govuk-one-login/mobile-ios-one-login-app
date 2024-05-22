6328fdc72e973df3c6d8a401a3142a0f0e3d1650
mcgarj <joe.mcgarry@digital.cabinet-office.gov.uk>
2024-05-22 16:10:49 +0100

    chore: embeds elements in vstack, still having layout issues

--- Sources/Screens/SignOutConfirmation/SignOutViewModel.swift
+++ Sources/Screens/SignOutConfirmation/SignOutViewModel.swift
@@ -2,14 +2,13 @@ import GDSCommon
 import UIKit
 
 struct SignOutPage: GDSInstructionsViewModel, BaseViewModel {
-    var rightBarButtonTitle: GDSCommon.GDSLocalisedString?
+    var rightBarButtonTitle: GDSLocalisedString? = "close"
 
-    var backButtonIsHidden: Bool
+    var backButtonIsHidden: Bool = true
 
     let title: GDSLocalisedString = "app_signOutConfirmationTitle"
     let body: String = GDSLocalisedString(stringLiteral: "app_signOutConfirmationBody2").value
     let childView = UIView()
-
     var label = UILabel()
     let body2: String = GDSLocalisedString(stringLiteral: "app_signOutConfirmationBody2").value
     let body3: String = GDSLocalisedString(stringLiteral: "app_signOutConfirmationBody3").value
@@ -19,30 +18,26 @@ struct SignOutPage: GDSInstructionsViewModel, BaseViewModel {
                                              GDSLocalisedString(stringLiteral: "app_signOutConfirmationBullet2").value,
                                              GDSLocalisedString(stringLiteral: "app_signOutConfirmationBullet3").value
                                             ])
+    var vStack: UIStackView
     let buttonViewModel: ButtonViewModel
     let secondaryButtonViewModel: (any ButtonViewModel)? = nil
 
     init(buttonAction: @escaping () -> Void) {
-//        self.childView.translatesAutoresizingMaskIntoConstraints = false
         label.text = "\(body2) \n\(body3)"
-        self.childView.addSubview(bulletView)
-        self.childView.addSubview(label)
-//        NSLayoutConstraint.activate([
-//            label.topAnchor.constraint(equalTo: bulletView.bottomAnchor, constant: 10),
-//            label.leadingAnchor.constraint(equalTo: childView.leadingAnchor, constant: 10),
-//            label.trailingAnchor.constraint(equalTo: childView.trailingAnchor, constant: -10)
-//        ])
+        self.vStack = UIStackView(arrangedSubviews: [bulletView, label])
+        self.childView.addSubview(vStack)
+
         self.buttonViewModel = StandardButtonViewModel(titleKey: "app_signOutAndDeleteAppDataButton") {
             buttonAction()
         }
     }
 
     func didAppear() {
-        <#code#>
+
     }
 
     func didDismiss() {
-        <#code#>
+
     }
 
     func addToChildView() {
