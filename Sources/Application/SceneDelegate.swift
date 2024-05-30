20778563b71e03e7dec9dec1d279e8c50c205024
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2024-05-30 15:42:00 +0100

    fix: linting

--- Sources/Application/SceneDelegate.swift
+++ Sources/Application/SceneDelegate.swift
@@ -17,7 +17,6 @@ class SceneDelegate: UIResponder,
     func scene(_ scene: UIScene,
                willConnectTo session: UISceneSession,
                options connectionOptions: UIScene.ConnectionOptions) {
-        
         guard let windowScene = (scene as? UIWindowScene) else {
             fatalError("Window failed to initialise in SceneDelegate")
         }
