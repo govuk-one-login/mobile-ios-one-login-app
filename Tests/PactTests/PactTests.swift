f5c7428744ea189809301f81e1dc5cd46885b12a
mcgarj <joe.mcgarry@digital.cabinet-office.gov.uk>
2024-07-04 09:25:12 +0100

    refactor: changes response and request to codable object

--- Tests/PactTests/PactTests.swift
+++ Tests/PactTests/PactTests.swift
@@ -7,13 +7,15 @@ final class PactTests: XCTestCase {
     var mockService: MockService!
     var networkClient: NetworkClient?
     var request: URLRequest!
+    var requestBody: MockRequestBody!
+    var responseBody: MockResponseBody!
 
     override func setUp() {
         super.setUp()
 
         request = URLRequest(url: URL(string: "http://localhost:1234/pact-test")!)
-        let bodyDict = ["request": "mock_request"]
-        let encoded = try? JSONEncoder().encode(bodyDict)
+        let requestBody = ["request": "mock_request"]
+        let encoded = try? JSONEncoder().encode(requestBody)
         request.httpBody = encoded
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
@@ -25,6 +27,9 @@ final class PactTests: XCTestCase {
         request = nil
         mockService = nil
         networkClient = nil
+        request = nil
+        requestBody = nil
+        responseBody = nil
     }
 
     func testMockRequest() {
---
+++ Tests/PactTests/PactTypes.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+struct MockRequestBody: Codable {
+    let body: [String: String]
+}
+
+struct MockResponseBody: Codable {
+    let body: [String: String]
+}
