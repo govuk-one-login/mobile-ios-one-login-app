e607c902d8665a18c4db7c20680531188a8fadde
mcgarj <joe.mcgarry@digital.cabinet-office.gov.uk>
2024-07-04 10:50:08 +0100

    refactor: tidying up code, removing duplicate test plan

--- Tests/PactTests/PactTests.swift
+++ Tests/PactTests/PactTests.swift
@@ -7,19 +7,18 @@ final class PactTests: XCTestCase {
     var mockService: MockService!
     var networkClient: NetworkClient?
     var request: URLRequest!
-    var requestBody: MockRequestBody!
-    var responseBody: MockResponseBody!
 
     override func setUp() {
         super.setUp()
 
         request = URLRequest(url: URL(string: "http://localhost:1234/pact-test")!)
-        let requestBody = ["request": "mock_request"]
+        let requestBody = MockRequestBody(request: "mock_request")
         let encoded = try? JSONEncoder().encode(requestBody)
         request.httpBody = encoded
         request.httpMethod = "POST"
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        mockService = MockService(provider: "Mobile:MobilePlatform:DummyProvider", consumer: "OneLogin App")
+        mockService = MockService(provider: "Mobile:MobilePlatform:DummyProvider",
+                                  consumer: "OneLogin App")
         networkClient = NetworkClient()
     }
 
@@ -28,14 +27,14 @@ final class PactTests: XCTestCase {
         mockService = nil
         networkClient = nil
         request = nil
-        requestBody = nil
-        responseBody = nil
     }
 
     func testMockRequest() {
         mockService
             .uponReceiving("A Mock Request")
-            .withRequest(method: .POST, path: "/pact-test", headers: ["Content-Type": "application/json"], body: ["request": "mock_request"])
+            .withRequest(method: .POST, path: "/pact-test",
+                         headers: ["Content-Type": "application/json"],
+                         body: ["request": "mock_request"])
             .willRespondWith(status: 200,
                              headers: ["Content-Type": "application/json"],
                              body: [ "response": "mock_response" ])
@@ -43,8 +42,8 @@ final class PactTests: XCTestCase {
         mockService.run(timeout: 60) { (testComplete) in
             Task {
                 let result = try await self.networkClient?.makeRequest(self.request)
-                let resultDict = try JSONDecoder().decode([String: String].self, from: result!)
-                XCTAssertEqual(resultDict["response"], "mock_response")
+                let mockResponse = try JSONDecoder().decode(MockResponseBody.self, from: result!)
+                XCTAssertEqual(mockResponse.response, "mock_response")
                 testComplete()
             }
         }
