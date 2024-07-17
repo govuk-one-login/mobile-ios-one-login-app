d1ee727fcc551c3c0ce1596d10b0d50250cbc4c2
mcgarj <joe.mcgarry@digital.cabinet-office.gov.uk>
2024-07-17 10:27:54 +0100

    chore: added sleep back in, comments out linting in pact step. Reverts Pact tests to mock tests only as requested by back end

--- Tests/PactTests/PactTests.swift
+++ Tests/PactTests/PactTests.swift
@@ -14,7 +14,7 @@ final class PactTests: XCTestCase {
         super.setUp()
         
         mockTokenVerifier = MockTokenVerifier()
-        mockService = MockService(provider: "Mobile:MobilePlatform:StsBackendApi",
+        mockService = MockService(provider: "Mobile:MobilePlatform:DummyProvider",
                                   consumer: "OneLogin App")
         networkClient = NetworkClient()
     }
@@ -53,88 +53,88 @@ final class PactTests: XCTestCase {
     }
     
     // swiftlint:disable line_length
-    func testTokenExchangeRequest() {
-        var request = URLRequest(url: URL(string: "http://localhost:1234/token")!)
-        let tokenExchangeRequest = request.tokenExchange(subjectToken: "subjectToken", scope: "sts-test.hello-world.read")
-
-        mockService
-            .uponReceiving("A token exchange request")
-            .withRequest(method: .POST, path: "/token",
-                         headers: ["Content-Type": "application/x-www-form-urlencoded"],
-                         body: "grant_type=urn:ietf:params:oauth:grant-type:token-exchange&scope=sts-test.hello-world.read&subject_token=subjectToken&subject_token_type=urn:ietf:params:oauth:token-type:access_token")
-            .willRespondWith(status: 200,
-                             headers: ["Content-Type": "application/json"],
-                             body: [ "access_token": "accessToken",
-                                     "token_type": "Bearer",
-                                     "expires_in": 180])
-
-        mockService.run(timeout: 60) { [tokenExchangeRequest] (testComplete) in
-            Task {
-                let result = try await self.networkClient?.makeRequest(tokenExchangeRequest)
-                let jsonDecoder = JSONDecoder()
-                jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
-
-                let tokenResponse = try jsonDecoder.decode(ServiceTokenResponse.self, from: result!)
-                XCTAssertEqual(tokenResponse.accessToken, "accessToken")
-                XCTAssertEqual(tokenResponse.tokenType, "Bearer")
-                XCTAssertEqual(tokenResponse.expiresIn, 180)
-                testComplete()
-            }
-        }
-    }
+//    func testTokenExchangeRequest() {
+//        var request = URLRequest(url: URL(string: "http://localhost:1234/token")!)
+//        let tokenExchangeRequest = request.tokenExchange(subjectToken: "subjectToken", scope: "sts-test.hello-world.read")
+//
+//        mockService
+//            .uponReceiving("A token exchange request")
+//            .withRequest(method: .POST, path: "/token",
+//                         headers: ["Content-Type": "application/x-www-form-urlencoded"],
+//                         body: "grant_type=urn:ietf:params:oauth:grant-type:token-exchange&scope=sts-test.hello-world.read&subject_token=subjectToken&subject_token_type=urn:ietf:params:oauth:token-type:access_token")
+//            .willRespondWith(status: 200,
+//                             headers: ["Content-Type": "application/json"],
+//                             body: [ "access_token": "accessToken",
+//                                     "token_type": "Bearer",
+//                                     "expires_in": 180])
+//
+//        mockService.run(timeout: 60) { [tokenExchangeRequest] (testComplete) in
+//            Task {
+//                let result = try await self.networkClient?.makeRequest(tokenExchangeRequest)
+//                let jsonDecoder = JSONDecoder()
+//                jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+//
+//                let tokenResponse = try jsonDecoder.decode(ServiceTokenResponse.self, from: result!)
+//                XCTAssertEqual(tokenResponse.accessToken, "accessToken")
+//                XCTAssertEqual(tokenResponse.tokenType, "Bearer")
+//                XCTAssertEqual(tokenResponse.expiresIn, 180)
+//                testComplete()
+//            }
+//        }
+//    }
     // swiftlint:enable line_length
-    @MainActor
-    func testAuthorizationRequest() throws {
-        let mockIDToken = MockJWKSResponse.idToken
-        
-        var request = URLRequest(url: URL(string: "http://localhost:1234/token")!)
-        request.httpMethod = "POST"
-        request.setValue("application/x-www-form-urlencoded; charset=UTF-8", forHTTPHeaderField: "Content-Type")
-        
-        var urlParser = URLComponents()
-        urlParser.queryItems = [
-            URLQueryItem(name: "code", value: "code"),
-            URLQueryItem(name: "code_verifier", value: "jM6oX_w_wU4biA2hDwmy0nkC_JsgvJRxaX0D-w1Hou0"),
-            URLQueryItem(name: "redirect_uri", value: "https://mobile.build.account.gov.uk/redirect"),
-            URLQueryItem(name: "client_id", value: "bYrcuRVvnylvEgYSSbBjwXzHrwJ"),
-            URLQueryItem(name: "grant_type", value: "authoriztion_code")
-        ]
-        let body = urlParser.percentEncodedQuery?.data(using: .utf8)
-        request.httpBody = body
-
-        mockService
-            .given("mock_auth_code is a valid authorization code")
-            .given("https://mock-redirect-uri.gov.uk is the redirect URI used in the authorization request")
-            .given("the code_challenge sent in the authorization request matches the verifier mock_code_verifier")
-            .uponReceiving("An authorization request")
-            .withRequest(method: .POST, path: "/token",
-                         headers: ["Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"],
-                         body: urlParser.percentEncodedQuery)
-            .willRespondWith(status: 200,
-                             headers: ["Content-Type": "application/json"],
-                             body: [ "access_token": Matcher.somethingLike("accessToken"),
-                                     "id_token": Matcher.somethingLike(mockIDToken),
-                                     "token_type": Matcher.somethingLike("Bearer"),
-                                     "expiry_date": Matcher.somethingLike(180)])
-
-        mockService.run(timeout: 10) { [request] (testComplete) in
-
-            Task {
-                let result = try await self.networkClient?.makeRequest(request)
-                let jsonDecoder = JSONDecoder()
-                jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
-                jsonDecoder.dateDecodingStrategy = .secondsSince1970
-                
-                let tokenResponse = try jsonDecoder.decode(TokenResponse.self, from: result!)
-                let decodedIDToken = try self.mockTokenVerifier.extractPayload(tokenResponse.idToken!)
-
-                XCTAssertEqual(decodedIDToken?.aud, MockTokenVerifier.mockPayload.aud)
-                XCTAssertEqual(tokenResponse.accessToken, "accessToken")
-                XCTAssertEqual(tokenResponse.tokenType, "Bearer")
-                XCTAssertEqual(tokenResponse.expiryDate, Date(timeIntervalSince1970: 180))
-                testComplete()
-            }
-            
-        }
-    }
+//    @MainActor
+//    func testAuthorizationRequest() throws {
+//        let mockIDToken = MockJWKSResponse.idToken
+//        
+//        var request = URLRequest(url: URL(string: "http://localhost:1234/token")!)
+//        request.httpMethod = "POST"
+//        request.setValue("application/x-www-form-urlencoded; charset=UTF-8", forHTTPHeaderField: "Content-Type")
+//        
+//        var urlParser = URLComponents()
+//        urlParser.queryItems = [
+//            URLQueryItem(name: "code", value: "code"),
+//            URLQueryItem(name: "code_verifier", value: "jM6oX_w_wU4biA2hDwmy0nkC_JsgvJRxaX0D-w1Hou0"),
+//            URLQueryItem(name: "redirect_uri", value: "https://mobile.build.account.gov.uk/redirect"),
+//            URLQueryItem(name: "client_id", value: "bYrcuRVvnylvEgYSSbBjwXzHrwJ"),
+//            URLQueryItem(name: "grant_type", value: "authoriztion_code")
+//        ]
+//        let body = urlParser.percentEncodedQuery?.data(using: .utf8)
+//        request.httpBody = body
+//
+//        mockService
+//            .given("mock_auth_code is a valid authorization code")
+//            .given("https://mock-redirect-uri.gov.uk is the redirect URI used in the authorization request")
+//            .given("the code_challenge sent in the authorization request matches the verifier mock_code_verifier")
+//            .uponReceiving("An authorization request")
+//            .withRequest(method: .POST, path: "/token",
+//                         headers: ["Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"],
+//                         body: urlParser.percentEncodedQuery)
+//            .willRespondWith(status: 200,
+//                             headers: ["Content-Type": "application/json"],
+//                             body: [ "access_token": Matcher.somethingLike("accessToken"),
+//                                     "id_token": Matcher.somethingLike(mockIDToken),
+//                                     "token_type": Matcher.somethingLike("Bearer"),
+//                                     "expiry_date": Matcher.somethingLike(180)])
+//
+//        mockService.run(timeout: 10) { [request] (testComplete) in
+//
+//            Task {
+//                let result = try await self.networkClient?.makeRequest(request)
+//                let jsonDecoder = JSONDecoder()
+//                jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+//                jsonDecoder.dateDecodingStrategy = .secondsSince1970
+//                
+//                let tokenResponse = try jsonDecoder.decode(TokenResponse.self, from: result!)
+//                let decodedIDToken = try self.mockTokenVerifier.extractPayload(tokenResponse.idToken!)
+//
+//                XCTAssertEqual(decodedIDToken?.aud, MockTokenVerifier.mockPayload.aud)
+//                XCTAssertEqual(tokenResponse.accessToken, "accessToken")
+//                XCTAssertEqual(tokenResponse.tokenType, "Bearer")
+//                XCTAssertEqual(tokenResponse.expiryDate, Date(timeIntervalSince1970: 180))
+//                testComplete()
+//            }
+//            
+//        }
+//    }
 }
