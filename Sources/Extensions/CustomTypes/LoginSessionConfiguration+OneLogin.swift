b08651a4a2f4df9e4e07cf79f562a290b776a6c7
Dylan James <90717175+DylanRJ@users.noreply.github.com>
2024-07-18 10:18:04 +0100

    feat: send persistent session ID in `/authorize` request (#164)
    * feat: basic implementation for the ticket requirements
    
    * test: adding test to AuthenticationCoordinatorTests
    
    * test: adjusting test assertion placement
    
    * test: removing flakiness in DeveloperMenuViewControllerTests
    
    * build: update package reference

--- Sources/Extensions/CustomTypes/LoginSessionConfiguration+OneLogin.swift
+++ Sources/Extensions/CustomTypes/LoginSessionConfiguration+OneLogin.swift
@@ -2,13 +2,14 @@ import Authentication
 import Foundation
 
 extension LoginSessionConfiguration {
-    static var oneLogin: LoginSessionConfiguration {
+    static func oneLogin(persistentSessionId: String? = nil) -> Self {
         let env = AppEnvironment.self
         return .init(authorizationEndpoint: env.callingSTSEnabled ? env.stsAuthorize : env.oneLoginAuthorize,
                      tokenEndpoint: env.callingSTSEnabled ? env.stsToken : env.oneLoginToken,
                      scopes: [.openid],
                      clientID: env.callingSTSEnabled ? env.stsClientID : env.oneLoginClientID,
                      redirectURI: env.oneLoginRedirect,
-                     locale: env.isLocaleWelsh ? .cy : .en)
+                     locale: env.isLocaleWelsh ? .cy : .en,
+                     persistentSessionId: persistentSessionId)
     }
 }
