0d0c30a51df702bcc971ee953c3527c65eb28337
Oliver BINNS <oliver.binns@digital.cabinet-office.gov.uk>
2024-08-27 15:07:26 +0100

    chore: added additional tests

--- Tests/UnitTests/Utilities/Session/PersistentSessionManagerTests.swift
+++ Tests/UnitTests/Utilities/Session/PersistentSessionManagerTests.swift
@@ -43,4 +43,35 @@ extension PersistentSessionManagerTests {
         XCTAssertFalse(sut.isReturningUser)
         XCTAssertFalse(sut.isPersistentSessionIDMissing)
     }
+
+    func testStartSession_logsTheUserIn() async throws {
+        // GIVEN I am not logged in
+        let loginSession = MockLoginSession()
+        // WHEN I start a session
+        try await sut.startSession(using: loginSession)
+        // THEN a login screen is shown
+        XCTAssertTrue(loginSession.didCallPerformLoginFlow)
+        // AND no persistent session ID is provided
+        let configuration = try XCTUnwrap(loginSession.sessionConfiguration)
+        XCTAssertNil(configuration.persistentSessionId)
+    }
+
+    func testStartSession_reauthenticatesTheUser() async throws {
+        // GIVEN my session has expired
+        let persistentSessionID = UUID().uuidString
+        unprotectedStore.set(persistentSessionID, forKey: .persistentSessionID)
+
+        let loginSession = MockLoginSession()
+        // WHEN I start a session
+        do {
+            try await sut.startSession(using: loginSession)
+        } catch {
+
+        }
+        // THEN a login screen is shown
+        XCTAssertTrue(loginSession.didCallPerformLoginFlow)
+        // AND a persistent session ID is provided
+        let configuration = try XCTUnwrap(loginSession.sessionConfiguration)
+        XCTAssertEqual(configuration.persistentSessionId, persistentSessionID)
+    }
 }
