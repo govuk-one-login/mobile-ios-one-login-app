86f6a99c694198d5351cd838e67b08015d6e1c7f
dylanjames-dd <dylan.james@digital.cabinet-office.gov.uk>
2024-09-18 09:04:56 +0100

    ci: swiftlint failures (#196)
    * fix: swiftlint failures
    
    * fix: optional return type

--- Sources/Utilities/JWT/JWKInfo.swift
+++ Sources/Utilities/JWT/JWKInfo.swift
@@ -12,8 +12,10 @@ struct JWKSInfo: Codable {
     }
     
     func jwkForKID(_ kid: String) throws -> JWK? {
-        guard let jwkInfo = self.keys.first(where: { $0.kid  == kid }) else { return nil }
-        let json = try jwkInfo.jsonString
+        guard let jwkInfo = self.keys.first(where: { $0.kid  == kid }),
+              let json = try jwkInfo.jsonString else {
+            return nil
+        }
         return try JWK(json: json)
     }
 }
@@ -41,10 +43,10 @@ struct JWKInfo: Codable {
         case y
     }
     
-    var jsonString: String {
+    var jsonString: String? {
         get throws {
             let jsonData = try JSONEncoder().encode(self)
-            return String(decoding: jsonData, as: UTF8.self)
+            return String(data: jsonData, encoding: .utf8)
         }
     }
 }
