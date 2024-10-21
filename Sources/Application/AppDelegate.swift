be3ae6baf998f5bb42b87db56a30353e40f244e9
magdaline <magdaline.amoakohene@digital.cabinet-office.gov.uk>
2024-10-21 11:46:22 +0100

    ci: added debug token to workflow

--- Sources/Application/AppDelegate.swift
+++ Sources/Application/AppDelegate.swift
@@ -8,7 +8,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         GAnalytics().configure()
-        print(ProcessInfo.processInfo.environment)
         return true
     }
     
