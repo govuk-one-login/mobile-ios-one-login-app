d136a276b8d18cbe17f458fd9c83e1ab3f87a83b
magdaline <magdaline.amoakohene@digital.cabinet-office.gov.uk>
2024-12-04 14:50:34 +0000

    chore: removing docC

--- AppIntegrity/Sources/AppIntegrity/Protocols/ProofTokenGenerator.swift
+++ AppIntegrity/Sources/AppIntegrity/Protocols/ProofTokenGenerator.swift
@@ -1,6 +1,3 @@
-/// Generates a Proof of Possession JWT.
-/// When called, this engages the `AppIntegrityJWT` values found in the TokenGenerator package and signs the JWT using the CryptoSigningService.
-
 public protocol ProofTokenGenerator {
     var token: String { get throws }
 }
