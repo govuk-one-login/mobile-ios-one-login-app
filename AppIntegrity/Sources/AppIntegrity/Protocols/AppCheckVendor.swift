19fec163a3b1a6a92589b0cb154d1d3eba128a73
magdaline <magdaline.amoakohene@digital.cabinet-office.gov.uk>
2024-12-04 11:12:10 +0000

    docs: added detail to app integrity package

--- AppIntegrity/Sources/AppIntegrity/Protocols/AppCheckVendor.swift
+++ AppIntegrity/Sources/AppIntegrity/Protocols/AppCheckVendor.swift
@@ -1,5 +1,13 @@
 import FirebaseAppCheck
 
+/// This protocol is used to initialize a provider depending on the environment it is being used in:
+/// - Debug: AppCheckDebugProviderFactory
+/// - iOS 14 and above: AppAttestProviderFactory
+/// - Below iOS 14: DeviceCheckProviderFactory
+///
+/// The`token` function is then used to get a token from the provider.
+/// The token retrieved is then used in the network call for the client attestation.
+
 protocol AppCheckVendor {
     static func setAppCheckProviderFactory(_ factory: AppCheckProviderFactory?)
     static func appCheck() -> Self
