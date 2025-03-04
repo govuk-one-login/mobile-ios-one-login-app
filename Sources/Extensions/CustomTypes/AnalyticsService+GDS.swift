822b19770003d89ffe6eec237015d73989942893
magdaline <magdaline.amoakohene@digital.cabinet-office.gov.uk>
2025-03-04 16:39:45 +0000

    chore: change based on pr comment

--- Sources/Extensions/CustomTypes/AnalyticsService+GDS.swift
+++ Sources/Extensions/CustomTypes/AnalyticsService+GDS.swift
@@ -29,8 +29,6 @@ extension AnalyticsService {
             switch appTaxonomy {
             case .reauth:
                 (AppTaxonomy.login.rawValue, appTaxonomy.rawValue)
-            case .settings:
-                (appTaxonomy.rawValue, "undefined")
             default:
                 (appTaxonomy.rawValue, "undefined")
             }
