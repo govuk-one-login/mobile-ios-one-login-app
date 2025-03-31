96dcb91e4699f89e899504722ef34ae166485d27
magdaline <magdaline.amoakohene@digital.cabinet-office.gov.uk>
2025-03-31 13:36:18 +0100

    fix: adding accessibility hint to analytics opt-in page

--- Sources/Views/Buttons/StandardButtonViewModel.swift
+++ Sources/Views/Buttons/StandardButtonViewModel.swift
@@ -5,16 +5,19 @@ struct StandardButtonViewModel: ButtonViewModel {
     let icon: ButtonIconViewModel?
     let shouldLoadOnTap: Bool
     let action: (() -> Void)
+    let accessibilityHint: GDSLocalisedString?
     
     init(titleKey: String,
          titleStringVariableKeys: String...,
          icon: ButtonIconViewModel? = nil,
          shouldLoadOnTap: Bool = false,
+         accessibilityHint: GDSLocalisedString? = nil,
          action: @escaping () -> Void) {
         self.title = GDSLocalisedString(stringKey: titleKey,
                                         variableKeys: titleStringVariableKeys)
         self.icon = icon
         self.shouldLoadOnTap = shouldLoadOnTap
+        self.accessibilityHint = accessibilityHint
         self.action = action
     }
 }
