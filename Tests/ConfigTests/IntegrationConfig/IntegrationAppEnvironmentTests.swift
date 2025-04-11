d870d61325aa85026eb3d3e12e8565a6bf8a5077
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2025-04-11 15:03:58 +0100

    test: adding integration config tests

--- Tests/ConfigTests/IntegrationConfig/IntegrationAppEnvironmentTests.swift
+++ Tests/ConfigTests/IntegrationConfig/IntegrationAppEnvironmentTests.swift
@@ -1,3 +1,66 @@
 import XCTest
 
-final class IntegrationAppEnvironmentTests: XCTestCase { }
+final class IntegrationAppEnvironmentTests: XCTestCase {
+    func test_defaultEnvironment_retrieveFromPlist() throws {
+        let sut = AppEnvironment.self
+        
+        XCTAssertEqual(Bundle.main.bundleIdentifier, "uk.gov.onelogin.integration")
+        XCTAssertEqual(Bundle.main.infoDictionary?["CFBundleDisplayName"] as? String, "One Login - Integration")
+        
+        // Feature Flags
+        XCTAssertFalse(sut.walletVisibleToAll)
+        XCTAssertFalse(sut.walletVisibleIfExists)
+        XCTAssertFalse(sut.walletVisibleViaDeepLink)
+        XCTAssertFalse(sut.criOrchestratorEnabled)
+        
+        // Helpers
+        XCTAssertEqual(sut.buildConfiguration, "Integration")
+        XCTAssertEqual(sut.isLocaleWelsh, false)
+        XCTAssertEqual(sut.localeString, "en")
+        
+        // STS
+        XCTAssertEqual(sut.stsClientID, "3Do0FOcrpsXe-mMklRruPUWmjr8")
+        XCTAssertEqual(sut.stsBaseURLString, "token.integration.account.gov.uk")
+        XCTAssertEqual(sut.stsBaseURL.absoluteString, "https://token.integration.account.gov.uk")
+        XCTAssertEqual(sut.stsAuthorize.absoluteString, "https://token.integration.account.gov.uk/authorize")
+        XCTAssertEqual(sut.stsToken.absoluteString, "https://token.integration.account.gov.uk/token")
+        XCTAssertEqual(sut.stsHelloWorld.absoluteString, "https://hello-world.token.integration.account.gov.uk/hello-world")
+        XCTAssertEqual(sut.jwksURL.absoluteString, "https://token.integration.account.gov.uk/.well-known/jwks.json")
+        
+        // Mobile BE
+        XCTAssertEqual(sut.mobileBaseURLString, "mobile.integration.account.gov.uk")
+        XCTAssertEqual(sut.mobileBaseURL.absoluteString, "https://mobile.integration.account.gov.uk")
+        XCTAssertEqual(sut.mobileRedirect.absoluteString, "https://mobile.integration.account.gov.uk/redirect")
+        XCTAssertEqual(sut.appInfoURL.absoluteString, "https://mobile.integration.account.gov.uk/appInfo")
+        XCTAssertEqual(sut.txma.absoluteString, "https://mobile.integration.account.gov.uk/txma-event")
+        XCTAssertEqual(sut.walletCredentialIssuer.absoluteString, "https://example-credential-issuer.mobile.integration.account.gov.uk")
+        
+        // ID Check
+        XCTAssertEqual(sut.idCheckDomainURL.absoluteString, "https://review-b.integration.account.gov.uk")
+        XCTAssertEqual(sut.idCheckBaseURL.absoluteString, "https://api-backend-api.review-b.integration.account.gov.uk")
+        XCTAssertEqual(sut.idCheckAsyncBaseURL.absoluteString, "https://sessions.review-b-async.integration.account.gov.uk")
+        XCTAssertEqual(sut.idCheckHandoffURL.absoluteString, "https://review-b.integration.account.gov.uk/dca/app/handoff?device=iphone")
+        XCTAssertEqual(sut.readIDURL.absoluteString, "https://readid.review-b.integration.account.gov.uk/odata/v1/ODataServlet")
+        XCTAssertEqual(sut.iProovURL.absoluteString, "wss://gds.rp.secure.iproov.me/ws")
+        
+        // External
+        XCTAssertEqual(sut.govURLString, "gov.uk")
+        XCTAssertEqual(sut.yourServicesLink, "home.account.gov.uk")
+        XCTAssertEqual(sut.externalBaseURLString, "signin.integration.account.gov.uk")
+        
+        // Settings Page
+        XCTAssertEqual(sut.manageAccountURL.absoluteString, "https://gov.uk/using-your-gov-uk-one-login")
+        XCTAssertEqual(sut.govURL.absoluteString, "https://gov.uk")
+        XCTAssertEqual(sut.govSupportURL.absoluteString, "https://home.integration.account.gov.uk")
+        XCTAssertEqual(sut.manageAccountURLEnglish.absoluteString, "https://gov.uk/using-your-gov-uk-one-login")
+        XCTAssertEqual(sut.manageAccountURLWelsh.absoluteString, "https://gov.uk/defnyddio-eich-gov-uk-one-login")
+        XCTAssertEqual(sut.appHelpURL.absoluteString, "https://gov.uk/one-login/app-help?lng=en")
+        XCTAssertEqual(sut.contactURL.absoluteString, "https://home.account.gov.uk/contact-gov-uk-one-login?lng=en")
+        XCTAssertEqual(sut.privacyPolicyURL.absoluteString, "https://signin.integration.account.gov.uk/privacy-notice?lng=en")
+        XCTAssertEqual(sut.accessibilityStatementURL.absoluteString, "https://signin.integration.account.gov.uk/accessibility-statement?lng=en")
+        
+        // App Store
+        XCTAssertEqual(sut.appStoreURL.absoluteString, "https://apps.apple.com")
+        XCTAssertEqual(sut.appStore.absoluteString, "https://apps.apple.com/gb.app.uk.gov.digital-identity")
+    }
+}
