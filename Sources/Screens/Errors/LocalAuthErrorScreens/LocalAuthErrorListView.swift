4b3c4e3889f890e2ef3cfec533556977421ace20
dylanjames-dd <dylan.james@digital.cabinet-office.gov.uk>
2025-04-29 15:22:17 +0100

    fix: wallet feature flags (#318)
    * fix: wallet feature flag names and matching appInfo
    
    * test: fixing tests
    
    * chore: updating to recommended settings
    
    * chore: removing unneeded schemes
    
    * fix: removing "bullet" from local auth error list view naming

--- Sources/Screens/Errors/LocalAuthErrorScreens/LocalAuthErrorListView.swift
+++ Sources/Screens/Errors/LocalAuthErrorScreens/LocalAuthErrorListView.swift
@@ -4,23 +4,23 @@ import LocalAuthenticationWrapper
 import UIKit
 
 @MainActor
-struct LocalAuthErrorBulletView: ScreenBodyItem {
+struct LocalAuthErrorListView: ScreenBodyItem {
     let uiView: UIView
     
     init(localAuthType: LocalAuthType) {
-        let localAuthErrorBulletViewModel = LocalAuthErrorBulletViewModel(localAuthType: localAuthType)
+        let localAuthErrorBulletViewModel = LocalAuthErrorListViewModel(localAuthType: localAuthType)
         self.uiView = ListView(viewModel: localAuthErrorBulletViewModel)
     }
 }
 
-struct LocalAuthErrorBulletViewModel: ListViewModel {
+struct LocalAuthErrorListViewModel: ListViewModel {
     let title: GDSLocalisedString? = "app_localAuthManagerErrorBody3"
     let titleConfig: TitleConfig? = (font: .body, isHeader: false)
     let listItemStrings: [GDSLocalisedString]
     
     init(localAuthType: LocalAuthType) {
         self.listItemStrings = [
-            LocalAuthErrorBulletViewModel.determineLocalAuthString(localAuthContext: localAuthType),
+            LocalAuthErrorListViewModel.determineLocalAuthString(localAuthContext: localAuthType),
             GDSLocalisedString(stringLiteral: "app_localAuthManagerErrorNumberedList2",
                                attributes: [("Turn Passcode On", [.font: UIFont.bodyBold])]),
             "app_localAuthManagerErrorNumberedList3"
