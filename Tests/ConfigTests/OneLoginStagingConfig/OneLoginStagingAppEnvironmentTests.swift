a6f556848496ffd4e646eef5a97b95bca22d32a5
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2025-05-07 18:19:15 +0100

    chore: renaming config tests and adding release config tests

--- Tests/ConfigTests/OneLoginStagingConfig/OneLoginStagingAppEnvironmentTests.swift
+++ Tests/ConfigTests/OneLoginStagingConfig/OneLoginStagingAppEnvironmentTests.swift
@@ -1,24 +1,30 @@
 import XCTest
 
-final class StagingAppEnvironmentTests: XCTestCase {
-    func test_defaultEnvironment_retrieveFromPlist() throws {
-        let sut = AppEnvironment.self
-        
+final class OneLoginStagingAppEnvironmentTests: XCTestCase {
+    let sut = AppEnvironment.self
+    
+    func test_plistValues() {
         XCTAssertEqual(Bundle.main.bundleIdentifier, "uk.gov.onelogin.staging")
         XCTAssertEqual(Bundle.main.infoDictionary?["CFBundleDisplayName"] as? String, "One Login - Staging")
         XCTAssertEqual(Bundle.main.infoDictionary?["MinimumOSVersion"] as? String, "15.0")
-        
+    }
+    
+    func test_appEnvironment_featureFlags() {
         // Feature Flags
         XCTAssertTrue(sut.walletVisibleViaDeepLink)
         XCTAssertTrue(sut.walletVisibleIfExists)
         XCTAssertTrue(sut.walletVisibleToAll)
         XCTAssertTrue(sut.criOrchestratorEnabled)
-        
+    }
+    
+    func test_appEnvironment_helpers() {
         // Helpers
         XCTAssertEqual(sut.buildConfiguration, "Staging")
         XCTAssertEqual(sut.isLocaleWelsh, false)
         XCTAssertEqual(sut.localeString, "en")
-        
+    }
+    
+    func test_appEnvironment_stsURLs() {
         // STS
         XCTAssertEqual(sut.stsClientID, "ctQpngJQrFFCrppZtYQFFoklHaq")
         XCTAssertEqual(sut.stsBaseURLString, "token.staging.account.gov.uk")
@@ -27,7 +33,9 @@ final class StagingAppEnvironmentTests: XCTestCase {
         XCTAssertEqual(sut.stsToken.absoluteString, "https://token.staging.account.gov.uk/token")
         XCTAssertEqual(sut.stsHelloWorld.absoluteString, "https://hello-world.token.staging.account.gov.uk/hello-world")
         XCTAssertEqual(sut.jwksURL.absoluteString, "https://token.staging.account.gov.uk/.well-known/jwks.json")
-        
+    }
+    
+    func test_appEnvironment_mobileBEURLs() {
         // Mobile BE
         XCTAssertEqual(sut.mobileBaseURLString, "mobile.staging.account.gov.uk")
         XCTAssertEqual(sut.mobileBaseURL.absoluteString, "https://mobile.staging.account.gov.uk")
@@ -35,7 +43,9 @@ final class StagingAppEnvironmentTests: XCTestCase {
         XCTAssertEqual(sut.appInfoURL.absoluteString, "https://mobile.staging.account.gov.uk/appInfo")
         XCTAssertEqual(sut.txma.absoluteString, "https://mobile.staging.account.gov.uk/txma-event")
         XCTAssertEqual(sut.walletCredentialIssuer.absoluteString, "https://example-credential-issuer.mobile.staging.account.gov.uk")
-        
+    }
+    
+    func test_appEnvironment_idCheckURLs() {
         // ID Check
         XCTAssertEqual(sut.idCheckDomainURL.absoluteString, "https://review-b.staging.account.gov.uk")
         XCTAssertEqual(sut.idCheckBaseURL.absoluteString, "https://api-backend-api.review-b.staging.account.gov.uk")
@@ -43,12 +53,16 @@ final class StagingAppEnvironmentTests: XCTestCase {
         XCTAssertEqual(sut.idCheckHandoffURL.absoluteString, "https://review-b.staging.account.gov.uk/dca/app/handoff?device=iphone")
         XCTAssertEqual(sut.readIDURLString, "https://readid-proxy.review-b-async.staging.account.gov.uk/odata/v1/ODataServlet/")
         XCTAssertEqual(sut.iProovURLString, "wss://gds.rp.secure.iproov.me/ws")
-        
+    }
+    
+    func test_appEnvironment_externalURLs() {
         // External
         XCTAssertEqual(sut.govURLString, "gov.uk")
         XCTAssertEqual(sut.yourServicesLink, "home.account.gov.uk")
         XCTAssertEqual(sut.externalBaseURLString, "signin.staging.account.gov.uk")
-        
+    }
+    
+    func test_appEnvironment_settingsURLs() {
         // Settings Page
         XCTAssertEqual(sut.manageAccountURL.absoluteString, "https://gov.uk/using-your-gov-uk-one-login")
         XCTAssertEqual(sut.govURL.absoluteString, "https://gov.uk")
@@ -59,7 +73,9 @@ final class StagingAppEnvironmentTests: XCTestCase {
         XCTAssertEqual(sut.contactURL.absoluteString, "https://home.account.gov.uk/contact-gov-uk-one-login?lng=en")
         XCTAssertEqual(sut.privacyPolicyURL.absoluteString, "https://signin.staging.account.gov.uk/privacy-notice?lng=en")
         XCTAssertEqual(sut.accessibilityStatementURL.absoluteString, "https://signin.staging.account.gov.uk/accessibility-statement?lng=en")
-        
+    }
+    
+    func test_appEnvironment_appStoreURLs() {
         // App Store
         XCTAssertEqual(sut.appStoreURL.absoluteString, "https://apps.apple.com")
         XCTAssertEqual(sut.appStore.absoluteString, "https://apps.apple.com/gb.app.uk.gov.digital-identity")
