3dd5068571bc4712582586d81693834b74f5669f
Kotsalidou, Chionia <chionia.kotsalidou@digital.cabinet-office.gov.uk>
2025-05-23 11:05:56 +0100

    test: update url tests

--- Tests/ConfigTests/OneLoginStagingConfig/OneLoginStagingAppEnvironmentTests.swift
+++ Tests/ConfigTests/OneLoginStagingConfig/OneLoginStagingAppEnvironmentTests.swift
@@ -68,7 +68,7 @@ final class OneLoginStagingAppEnvironmentTests: XCTestCase {
         XCTAssertEqual(sut.govSupportURL.absoluteString, "https://home.staging.account.gov.uk")
         XCTAssertEqual(sut.manageAccountURLEnglish.absoluteString, "https://gov.uk/using-your-gov-uk-one-login")
         XCTAssertEqual(sut.manageAccountURLWelsh.absoluteString, "https://gov.uk/defnyddio-eich-gov-uk-one-login")
-        XCTAssertEqual(sut.appHelpURL.absoluteString, "https://gov.uk/one-login/app-help?lng=en")
+        XCTAssertEqual(sut.appHelpURL.absoluteString, "https://gov.uk/guidance/proving-your-identity-with-the-govuk-one-login-app")
         XCTAssertEqual(sut.contactURL.absoluteString, "https://home.account.gov.uk/contact-gov-uk-one-login?lng=en")
         XCTAssertEqual(sut.privacyPolicyURL.absoluteString, "https://signin.staging.account.gov.uk/privacy-notice?lng=en")
         XCTAssertEqual(sut.accessibilityStatementURL.absoluteString, "https://gov.uk/one-login/app-accessibility")
