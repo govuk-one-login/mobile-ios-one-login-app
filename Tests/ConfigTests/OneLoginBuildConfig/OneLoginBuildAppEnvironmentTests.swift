3ec5e6968c70592d0977b64cf17e62db982f3238
Kotsalidou, Chionia <chionia.kotsalidou@digital.cabinet-office.gov.uk>
2025-05-26 12:09:07 +0100

    test: update privacy url tests

--- Tests/ConfigTests/OneLoginBuildConfig/OneLoginBuildAppEnvironmentTests.swift
+++ Tests/ConfigTests/OneLoginBuildConfig/OneLoginBuildAppEnvironmentTests.swift
@@ -70,7 +70,7 @@ final class OneLoginBuildAppEnvironmentTests: XCTestCase {
         XCTAssertEqual(sut.manageAccountURLWelsh.absoluteString, "https://gov.uk/defnyddio-eich-gov-uk-one-login")
         XCTAssertEqual(sut.appHelpURL.absoluteString, "https://gov.uk/one-login/app-help?lng=en")
         XCTAssertEqual(sut.contactURL.absoluteString, "https://home.account.gov.uk/contact-gov-uk-one-login?lng=en")
-        XCTAssertEqual(sut.privacyPolicyURL.absoluteString, "https://signin.build.account.gov.uk/privacy-notice?lng=en")
+        XCTAssertEqual(sut.privacyPolicyURL.absoluteString, "https://gov.uk/government/publications/govuk-one-login-privacy-notice")
         XCTAssertEqual(sut.accessibilityStatementURL.absoluteString, "https://gov.uk/one-login/app-accessibility")
     }
     
--- Tests/ConfigTests/OneLoginReleaseConfig/OneLoginReleaseAppEnvironmentTests.swift
+++ Tests/ConfigTests/OneLoginReleaseConfig/OneLoginReleaseAppEnvironmentTests.swift
@@ -70,7 +70,7 @@ final class OneLoginReleaseAppEnvironmentTests: XCTestCase {
         XCTAssertEqual(sut.manageAccountURLWelsh.absoluteString, "https://gov.uk/defnyddio-eich-gov-uk-one-login")
         XCTAssertEqual(sut.appHelpURL.absoluteString, "https://gov.uk/one-login/app-help?lng=en")
         XCTAssertEqual(sut.contactURL.absoluteString, "https://home.account.gov.uk/contact-gov-uk-one-login?lng=en")
-        XCTAssertEqual(sut.privacyPolicyURL.absoluteString, "https://signin.account.gov.uk/privacy-notice?lng=en")
+        XCTAssertEqual(sut.privacyPolicyURL.absoluteString, "https://gov.uk/government/publications/govuk-one-login-privacy-notice")
         XCTAssertEqual(sut.accessibilityStatementURL.absoluteString, "https://gov.uk/one-login/app-accessibility")
     }
     
