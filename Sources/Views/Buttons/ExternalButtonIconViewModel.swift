f7f91096365f1989c02c6e5f19382877e624b6e3
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2025-05-29 22:47:25 +0100

    test: adding small missing test file

--- Sources/Views/Buttons/ExternalButtonIconViewModel.swift
+++ Sources/Views/Buttons/ExternalButtonIconViewModel.swift
@@ -1,8 +1,8 @@
 import GDSCommon
 
 struct ExternalButtonIconViewModel: ButtonIconViewModel {
-    let iconName: String = ButtonIcon.arrowUpRight
-    let symbolPosition: SymbolPosition = .afterTitle
+    let iconName = ButtonIcon.arrowUpRight
+    let symbolPosition = SymbolPosition.afterTitle
 }
 
 extension ButtonIconViewModel where Self == ExternalButtonIconViewModel {
