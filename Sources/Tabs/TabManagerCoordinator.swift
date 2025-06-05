f2befba8feb4a450d64c47463ac46fce738a31cf
Kotsalidou, Chionia <chionia.kotsalidou@digital.cabinet-office.gov.uk>
2025-06-05 17:40:52 +0100

    refactor: make sure the flag is not cleared

--- Sources/Tabs/TabManagerCoordinator.swift
+++ Sources/Tabs/TabManagerCoordinator.swift
@@ -99,19 +99,16 @@ extension TabManagerCoordinator: ParentCoordinator {
         if child is SettingsCoordinator {
             Task {
                 do {
+                    let isWalletAccessed = WalletAvailabilityService.hasAccessedBefore
                     try await sessionManager.clearAllSessionData(restartLoginFlow: false)
                     
-                    await MainActor.run {
-                        let viewModel = SignOutSuccessfulViewModel(analyticsService: analyticsService,
-                                                                   withWallet: WalletAvailabilityService.hasAccessedBefore) {
-                            NotificationCenter.default.post(
-                                name: .didLogout
-                            )
-                        }
-                        let signOutSuccessful = GDSInformationViewController(viewModel: viewModel)
-                        signOutSuccessful.modalPresentationStyle = .fullScreen
-                        root.present(signOutSuccessful, animated: false)
+                    let viewModel = SignOutSuccessfulViewModel(analyticsService: analyticsService,
+                                                               withWallet: isWalletAccessed) {
+                        NotificationCenter.default.post(name: .didLogout)
                     }
+                    let signOutSuccessful = GDSInformationViewController(viewModel: viewModel)
+                    signOutSuccessful.modalPresentationStyle = .fullScreen
+                    root.present(signOutSuccessful, animated: false)
                 } catch {
                     let viewModel = SignOutErrorViewModel(analyticsService: analyticsService,
                                                           error: error)
