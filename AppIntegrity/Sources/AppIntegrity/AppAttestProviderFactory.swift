8faab40dec45f7fe8de487612c88bf14e87db793
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2025-07-30 10:01:53 +0100

    chore: removing extra available condition

--- AppIntegrity/Sources/AppIntegrity/AppAttestProviderFactory.swift
+++ AppIntegrity/Sources/AppIntegrity/AppAttestProviderFactory.swift
@@ -1,7 +1,6 @@
 import FirebaseAppCheck
 import FirebaseCore
 
-@available(iOS 14, *)
 final class AppAttestProviderFactory: NSObject,
                                       AppCheckProviderFactory {
     func createProvider(with app: FirebaseApp) -> AppCheckProvider? {
