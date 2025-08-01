a18caaf9afa29e72a3ca3881621f68623d19e4c8
dylanjames-dd <dylan.james@digital.cabinet-office.gov.uk>
2025-08-01 11:34:04 +0100

    fix: remove saved_doc_type from analytics parameters (#457)

--- Sources/Extensions/Dictionary+OneLogin.swift
+++ Sources/Extensions/Dictionary+OneLogin.swift
@@ -2,7 +2,6 @@ import Foundation
 
 extension Dictionary where Key == String, Value == Any {
     static let oneLoginDefaults = [
-        "saved_doc_type": "undefined",
         "primary_publishing_organisation": "government digital service - digital identity",
         "organisation": "<OT1056>",
         "taxonomy_level1": "one login mobile application",
