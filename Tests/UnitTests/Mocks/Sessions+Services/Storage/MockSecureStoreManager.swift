e5ac15bcee6ef79994ced8486f157fee86de70db
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2025-11-06 18:48:16 +0000

    test: passing test suite

--- Tests/UnitTests/Mocks/Sessions+Services/Storage/MockSecureStoreManager.swift
+++ Tests/UnitTests/Mocks/Sessions+Services/Storage/MockSecureStoreManager.swift
@@ -1,25 +1,55 @@
 @testable import OneLogin
+import SecureStore
 
 final class MockSecureStoreManager: SecureStoreManaging, SessionBoundData {
     var savedItems = [String: String]()
+    var didCallDeleteStore = false
+    var didCallClearSessionData = false
     
-    func clearSessionData() async throws {
-        
-    }
-    
-    func checkItemExists(_ itemName: String) -> Bool {
-        true
-    }
+    var errorFromSaveItem: Error?
+    var errorFromReadItem: Error?
+    var errorFromDeleteItem: Error?
+    var returnFromCheckItemExists = true
     
     func saveItem(_ item: String, itemName: String) throws {
-        
+        if let errorFromSaveItem {
+            throw errorFromSaveItem
+        } else {
+            savedItems[itemName] = item
+        }
     }
     
     func readItem(_ itemName: String) throws -> String {
-        ""
+        if let errorFromReadItem {
+            throw errorFromReadItem
+        } else {
+            guard let savedItem = savedItems[itemName] else {
+                throw SecureStoreError.unableToRetrieveFromUserDefaults
+            }
+            return savedItem
+        }
     }
     
     func deleteItem(_ itemName: String) {
-        
+        savedItems[itemName] = nil
+    }
+    
+    func delete() throws {
+        didCallDeleteStore = true
+        if let errorFromDeleteItem {
+            throw errorFromDeleteItem
+        }
+    }
+    
+    func checkItemExists(_ itemName: String) -> Bool {
+        if savedItems[itemName] != nil {
+            return true
+        }
+        return false
+    }
+    
+    func clearSessionData() {
+        didCallClearSessionData = true
+        savedItems = [:]
     }
 }
