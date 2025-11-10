3ceb899f55b5e119bd12a989b8bd085aa783489c
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2025-11-10 17:28:26 +0000

    feat: working on implementation

---
+++ Sources/Extensions/URLRequest+OneLogin.swift
@@ -0,0 +1,25 @@
+import Foundation
+import AppIntegrity
+
+enum RefreshExchangeError: Error {
+    case cantExtractAppIntegrityHeaders
+}
+
+extension URLRequest {
+    static func refreshExchange(
+        token: String,
+        appIntegrityProvider: AppIntegrityProvider
+    ) async throws -> URLRequest {
+        var request = URLRequest(url: AppEnvironment.stsToken)
+        request.httpMethod = "POST"
+        
+        var headers = try await appIntegrityProvider.integrityAssertions
+        headers["Content-Type"] = "application/x-www-form-urlencoded"
+        
+        request.allHTTPHeaderFields = headers
+        
+        request.httpBody = Data(base64URLEncoded: "grant_type=refresh_token&refresh_token=\(token)")
+
+        return request
+    }
+}
