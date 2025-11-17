2f06239552372f6f29c2a483e3052cf535ba04bf
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2025-11-17 14:21:39 +0000

    test: fix refresh token manager tests

--- Tests/UnitTests/Mocks/Sessions+Services/MockAppIntegrityProvider.swift
+++ Tests/UnitTests/Mocks/Sessions+Services/MockAppIntegrityProvider.swift
@@ -1,16 +1,14 @@
 import AppIntegrity
 
 final class MockAppIntegrityProvider: AppIntegrityProvider {
-    var integrityAssertions: [String: String]
     var errorThrownAssertingIntegrity: Error?
     
-    init(errorThrownAssertingIntegrity: Error? = nil) throws {
-        self.errorThrownAssertingIntegrity = errorThrownAssertingIntegrity
-        
-        if let error = errorThrownAssertingIntegrity {
-            throw error
-        } else {
-            self.integrityAssertions = [:]
+    var integrityAssertions: [String: String] {
+        get async throws {
+            if let errorThrownAssertingIntegrity {
+                throw errorThrownAssertingIntegrity
+            }
+            return ["testAsserion": "testValue"]
         }
     }
 }
