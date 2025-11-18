cdd909b4740f48ece1f903b6eecc109d560660bc
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2025-11-18 10:21:07 +0000

    chore: small corrections

--- Sources/Login/WebAuthenticationService.swift
+++ Sources/Login/WebAuthenticationService.swift
@@ -30,7 +30,7 @@ final class WebAuthenticationService: AuthenticationService {
             analyticsService.logEvent(ButtonEvent(textKey: "back"))
             throw error
         } catch let error as LoginErrorV2 where error.reason == .authorizationAccessDenied {
-            try await sessionManager.clearAllSessionData(restartLoginFlow: true)
+            try await sessionManager.clearAllSessionData(restartLoginFlow: false)
             throw error
         } catch {
             analyticsService.logCrash(error)
