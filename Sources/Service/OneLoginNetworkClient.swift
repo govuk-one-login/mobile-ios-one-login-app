d1482b32e6de07cb2a0893f368ae8d8df391f14d
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2025-11-28 16:00:33 +0000

    chore: bringing changes across from refresh token exchange manager

--- Sources/Service/OneLoginNetworkClient.swift
+++ Sources/Service/OneLoginNetworkClient.swift
@@ -13,8 +13,16 @@ protocol OneLoginNetworkClient {
     ) async throws -> Data
 }
 
+enum RefreshTokenExchangeError: Error {
+    case accountIntervention
+    case appIntegrityRetryError
+    case noInternet
+}
+
 final class NetworkingService: OneLoginNetworkingService, TokenExchangeManaging {
     let networkClient: NetworkClient
+    
+    private(set) var errorRetries = 0
     
     init(networkClient: NetworkClient = NetworkClient()) {
         self.networkClient = networkClient
@@ -34,23 +42,32 @@ final class NetworkingService: OneLoginNetworkingService, TokenExchangeManaging 
             
             return try JSONDecoder()
                 .decode(TokenResponse.self, from: exchangeResponse)
-        } catch let error as FirebaseAppCheckError where error.errorType == .network,
-                let error as FirebaseAppCheckError where error.errorType == .unknown,
-                let error as FirebaseAppCheckError where error.errorType == .generic {
+        } catch let error as FirebaseAppCheckError where error.errorType == .generic
+                    || error.errorType == .unknown {
+            guard errorRetries < 3 else {
+                throw RefreshTokenExchangeError.appIntegrityRetryError
+            }
+            errorRetries += 1
             return try await getUpdatedTokens(
                 refreshToken: refreshToken,
                 appIntegrityProvider: appIntegrityProvider
             )
-        } catch {
+        } catch let error as FirebaseAppCheckError where error.errorType == .network {
+            throw RefreshTokenExchangeError.noInternet
+        } catch let error as URLError where error.code == .notConnectedToInternet
+                    || error.code == .networkConnectionLost {
+            throw RefreshTokenExchangeError.noInternet
+        } catch let error as ServerError where error.errorCode == 400 {
             NotificationCenter.default.post(name: .accountIntervention)
-            throw error
+            throw RefreshTokenExchangeError.accountIntervention
         }
     }
     
     func makeRequest(_ request: URLRequest) async throws -> Data {
         do {
             return try await networkClient.makeRequest(request)
-        } catch let error as URLError where error.code == .notConnectedToInternet {
+        } catch let error as URLError where error.code == .notConnectedToInternet
+                    || error.code == .networkConnectionLost {
             throw error
         }
     }
@@ -64,7 +81,8 @@ final class NetworkingService: OneLoginNetworkingService, TokenExchangeManaging 
                 scope: scope,
                 request: request
             )
-        } catch let error as URLError where error.code == .notConnectedToInternet {
+        } catch let error as URLError where error.code == .notConnectedToInternet
+                    || error.code == .networkConnectionLost {
             throw error
         }
     }
