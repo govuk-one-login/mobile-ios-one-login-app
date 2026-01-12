748d42ca22057f3639830aecf5cfa97a1b4816e8
shukriahmed-dd <shukri.ahmed@digital.cabinet-office.gov.uk>
2026-01-12 14:22:06 +0000

    chore: create snapshot test target & plan

---
+++ Tests/SnapshotTests/OneLoginSnapshotTests.swift
@@ -0,0 +1,16 @@
+//
+//  OneLoginSnapshotTests.swift
+//  OneLoginSnapshotTests
+//
+//  Created by Ahmed, Shukri on 12/01/2026.
+//
+
+import Testing
+
+struct OneLoginSnapshotTests {
+
+    @Test func test() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
