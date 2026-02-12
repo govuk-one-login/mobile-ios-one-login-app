9c58c76c5daffa2d648ce78f0318af0b02626efc
Kotsalidou, Chionia <chionia.kotsalidou@digital.cabinet-office.gov.uk>
2026-02-12 15:32:25 +0000

    test: new test for account intervention

--- Tests/UnitTests/Utilities/Session/Authorization/TokenHolderTests.swift
+++ Tests/UnitTests/Utilities/Session/Authorization/TokenHolderTests.swift
@@ -1,6 +1,7 @@
 import MockNetworking
 @testable import Networking
 @testable import OneLogin
+import Testing
 import XCTest
 
 final class TokenHolderTests: XCTestCase {
@@ -136,5 +137,41 @@ extension TokenHolderTests {
         sut.update(accessToken: MockJWTs.genericToken, accessTokenExpiry: Date.distantPast)
         
         XCTAssertFalse(sut.isAccessTokenValid)
+    }
+    
+    func test_accountInterventionWithInvalidGrant() async throws {
+        let expectedToken = UUID().uuidString
+        MockURLProtocol.handler = {
+            let response = """
+            {
+                "access_token": "\(expectedToken)",
+                "token_type": "token",
+                "expires_in": 180
+            }
+            """
+            return (Data(response.utf8), HTTPURLResponse(statusCode: 200))
+        }
+        
+        // AND I have an valid access token
+        sut.update(accessToken: expectedToken, accessTokenExpiry: Date())
+        
+        let notification = NotificationCenter.default.notifications(named: .accountIntervention)
+        let iterator = notification.makeAsyncIterator()
+        
+        let jsonResponse = #"{ "error": "invalid_grant" }"#
+                
+        MockURLProtocol.handler = {
+            let data = Data(jsonResponse.utf8)
+            return (data, HTTPURLResponse(statusCode: 400))
+        }
+        
+        do {
+            _ = try await sut.fetchToken(withScope: "sts.hello-world.read")
+            Issue.record("Expect 400 error, but no error thrown")
+        } catch {
+            #expect((error as? ServerError)?.errorCode == 400)
+            let received = await iterator.next()?.name == .accountIntervention
+            #expect(received == true)
+        }
     }
 }
