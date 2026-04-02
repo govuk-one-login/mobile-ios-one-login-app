b31e5b95c1cb52d68995737b0604ed65c5d84484
Kotsalidou, Chionia <chionia.kotsalidou@digital.cabinet-office.gov.uk>
2026-04-02 13:15:58 +0100

    chore: update min version to 15 to be aligned with the project

--- MobilePlatformServices/Package.swift
+++ MobilePlatformServices/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MobilePlatformServices",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(name: "MobilePlatformServices", targets: ["MobilePlatformServices"])
     ],
