7cda37678da1d4ded03a57226cc9886f86b6518e
Markos Zoulias Charatzas <markos.charatzas@digital.cabinet-office.gov.uk>
2026-04-13 19:30:15 +0100

    feat: DCMAW-20025 improve execution time for OneLoginEnrolmentManagerTests
    	This code change ONLY modifies test source code.
    
        Replaced any `waitForTruth` calls that are based on an `NSPredicate` which is periodically evaluated
        with an XCTestExpectation that can be fulfilled. This significally reduces the time it takes since the expectation is
        fulffilled at almost the time it takes for the async code to execute. Whereas the periodic evaluation operates on
        1 second intervals with an expected minimum of 1 second. As a result, each test under `OneLoginEnrolmentManagerTests`
        takes milliseconds as opposed to 1 second each.

--- Tests/UnitTests/Utilities/OneLoginEnrolmentManagerTests.swift
+++ Tests/UnitTests/Utilities/OneLoginEnrolmentManagerTests.swift
@@ -81,43 +81,74 @@ extension OneLoginEnrolmentManagerTests {
     }
 
     func test_saveSession_fails() {
+        let expectation = expectation(description: #function)
         // GIVEN the user has given FaceID permission
+        let mockLocalAuthContext = MockLocalAuthManager()
         mockLocalAuthContext.userDidConsentToFaceID = true
         // GIVEN saveSession returns an uncaught error
+        let mockSessionManager = MockSessionManager()
+        let mockSessionManagerExpectation = MockSessionManagerExpectation(sessionManager: mockSessionManager, didSaveAuthSessionAsFunction: {
+            expectation.fulfill()
+        })
+        
         mockSessionManager.errorFromSaveSession = MockError.generic
+        let mockAnalyticsService = MockAnalyticsService()
+        let sut: OneLoginEnrolmentManager = .make(mockLocalAuthContext: mockLocalAuthContext, mockSessionManager: mockSessionManagerExpectation, mockAnalyticsService: mockAnalyticsService)
+        
         // WHEN saveSession is called
         sut.saveSession()
-        waitForTruth(self.mockSessionManager.didCallSaveSession, timeout: 5)
+        
+        self.wait(for: [expectation], timeout: 5)
+        XCTAssertTrue(mockSessionManager.didCallSaveSession)
         // THEN an error is recorded in Crashlytics
         XCTAssertEqual(mockAnalyticsService.crashesLogged, [MockError.generic as NSError])
     }
 
     func test_saveSession_promptForPermission_false() {
+        let expectation = expectation(description: #function)
         // GIVEN the user has already given FaceID permission
-        mockLocalAuthContext.userDidConsentToFaceID = false
+        let mockLocalAuthManager = MockLocalAuthManager()
+        let mockLocalAuthManagerExpectation = MockLocalAuthManagerExpectation(mockLocalAuthManager: mockLocalAuthManager,
+                                                                   expectation: expectation)
+        mockLocalAuthManager.userDidConsentToFaceID = false
+        let mockAnalyticsService = MockAnalyticsService()
+        let sut: OneLoginEnrolmentManager = .make(mockLocalAuthContext: mockLocalAuthManagerExpectation, mockAnalyticsService: mockAnalyticsService)
         // WHEN saveSession is called
         sut.saveSession()
-        waitForTruth(self.mockLocalAuthContext.didCallEnrolFaceIDIfAvailable, timeout: 5)
+        wait(for: [expectation], timeout: 5)
+        XCTAssertTrue(mockLocalAuthManager.didCallEnrolFaceIDIfAvailable)
         // THEN no error is recorded in Crashlytics
         XCTAssertEqual(mockAnalyticsService.crashesLogged, [])
     }
 
     func test_saveSession_promptForPermission_cancelled() {
+        let expectation = expectation(description: #function)
         // GIVEN promptForPermission throws a cancelled error
-        mockLocalAuthContext.errorFromEnrolLocalAuth = LocalAuthenticationWrapperError.cancelled
+        let mockLocalAuthManager = MockLocalAuthManager()
+        let mockLocalAuthManagerExpectation = MockLocalAuthManagerExpectation(mockLocalAuthManager: mockLocalAuthManager,
+                                                                   expectation: expectation)
+        mockLocalAuthManager.errorFromEnrolLocalAuth = LocalAuthenticationWrapperError.cancelled
+        let mockAnalyticsService = MockAnalyticsService()
+        let sut: OneLoginEnrolmentManager = .make(mockLocalAuthContext: mockLocalAuthManagerExpectation, mockSessionManager: mockSessionManager, mockAnalyticsService: mockAnalyticsService)
         // WHEN saveSession is called
         sut.saveSession()
-        waitForTruth(self.mockLocalAuthContext.didCallEnrolFaceIDIfAvailable, timeout: 5)
+        wait(for: [expectation], timeout: 5)
+        XCTAssertTrue(mockLocalAuthManager.didCallEnrolFaceIDIfAvailable)
         // THEN no error is recorded in Crashlytics
         XCTAssertEqual(mockAnalyticsService.crashesLogged, [])
     }
 
     func test_saveSession_promptForPermission_fails() {
+        let expectation = expectation(description: #function)
         // GIVEN promptForPermission throws an uncaught error
+        let mockLocalAuthContext = MockLocalAuthManager()
         mockLocalAuthContext.errorFromEnrolLocalAuth = MockError.generic
+        let mockAnalyticsService = MockAnalyticsServiceExpectation(expectation: expectation)
+        let sut: OneLoginEnrolmentManager = .make(mockLocalAuthContext: mockLocalAuthContext, mockSessionManager: mockSessionManager, mockAnalyticsService: mockAnalyticsService)
         // WHEN saveSession is called
         sut.saveSession()
-        waitForTruth(self.mockLocalAuthContext.didCallEnrolFaceIDIfAvailable, timeout: 5)
+        wait(for: [expectation], timeout: 5)
+        XCTAssertTrue(mockLocalAuthContext.didCallEnrolFaceIDIfAvailable)
         // THEN an error is recorded in Crashlytics
         XCTAssertEqual(mockAnalyticsService.crashesLogged, [MockError.generic as NSError])
     }
