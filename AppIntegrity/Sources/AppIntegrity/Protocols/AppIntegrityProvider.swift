21070fdeaddc158b3fd983d5abe0890a027e8011
shukriahmed-dd <shukri.ahmed@digital.cabinet-office.gov.uk>
2026-05-19 16:26:14 +0100

    feat: create new variables fetching assertions

--- AppIntegrity/Sources/AppIntegrity/Protocols/AppIntegrityProvider.swift
+++ AppIntegrity/Sources/AppIntegrity/Protocols/AppIntegrityProvider.swift
@@ -1,5 +1,8 @@
 import Foundation
 
 public protocol AppIntegrityProvider {
+    var clientAssertions: [String: String] { get async throws }
+    var dPopAssertion: [String: String] { get throws }
+    // TODO: DCMAW-20368 Delete this
     var integrityAssertions: [String: String] { get async throws }
 }
