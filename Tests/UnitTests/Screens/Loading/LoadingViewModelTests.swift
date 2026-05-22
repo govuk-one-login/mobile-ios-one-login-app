9c8f02df715c309e39b0a154d6100f7d169b5d3d
shukriahmed-dd <shukri.ahmed@digital.cabinet-office.gov.uk>
2026-05-22 13:31:10 +0100

    feat: DCMAW-19230 update loading screen to use design system (#816)
    * chore: import designsystem
    
    * feat: create screen using DS
    
    * chore: update tests
    
    * chore: update snapshot
    
    * chore: update ui library version
    
    * chore: delete snapshots
    
    * chore: add files

---
+++ Tests/UnitTests/Screens/Loading/LoadingViewModelTests.swift
@@ -0,0 +1,32 @@
+import DesignSystem
+import GDSAnalytics
+@testable import OneLogin
+import Testing
+
+@MainActor
+struct LoadingViewModelTests {
+    let mockAnalyticsService = MockAnalyticsService()
+    
+    @Test
+    func testScreen() {
+        let sut = LoadingViewModel(analyticsService: mockAnalyticsService)
+        #expect(sut.loadingLabelKey.stringKey == "app_loadingBody")
+    }
+    
+    @Test
+    func test_didAppear() {
+        let sut = LoadingViewModel(analyticsService: mockAnalyticsService)
+        let vc = GDSScreen(viewModel: sut)
+        
+        #expect(mockAnalyticsService.screenViews.count == 0)
+        vc.beginAppearanceTransition(true, animated: false)
+        vc.endAppearanceTransition()
+        #expect(mockAnalyticsService.screenViews.count == 1)
+        
+        let screen = ScreenView(id: IntroAnalyticsScreenID.loginLoading.rawValue,
+                                screen: IntroAnalyticsScreen.loginLoading,
+                                titleKey: "app_loadingBody")
+        #expect(mockAnalyticsService.screenViews as? [ScreenView] == [screen])
+        #expect(mockAnalyticsService.screenParamsLogged == screen.parameters)
+    }
+}
