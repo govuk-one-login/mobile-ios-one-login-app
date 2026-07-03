308163f11cc9fcda3ffc978c7293ae654d3e9e18
chionia-gds <chionia.kotsalidou@digital.cabinet-office.gov.uk>
2026-07-03 11:49:11 +0100

    feat: DCMAW-14660 Remove Common dependency from Unlock screen #862
    feat: DCMAW-14660 Remove Common dependency from Unlock screen

--- Sources/Screens/Unlock/UnlockScreenViewModel.swift
+++ Sources/Screens/Unlock/UnlockScreenViewModel.swift
@@ -1,34 +1,36 @@
+import DesignSystem
 import GDSAnalytics
-import GDSCommon
 import Logging
 import UIKit
 
 struct UnlockScreenViewModel: BaseViewModel {
-    let analyticsService: OneLoginAnalyticsService
-    let primaryButtonViewModel: ButtonViewModel
-    let accessibilityLabel: GDSLocalisedString = GDSLocalisedString(stringKey: "app_loadingLabel", "app_nameString")
-    
+    let didAppear: DesignSystem.Action?
+    let didDismiss: DesignSystem.Action? = nil
     let rightBarButtonTitle: GDSLocalisedString? = nil
     let backButtonIsHidden: Bool = true
+    let backButtonTitle: GDSLocalisedString? = nil
+    
+    let primaryButtonAction: () -> Void
+    let primaryButtonTitle: String = GDSLocalisedString(stringKey: "app_unlockButton").value
+    let accessibilityLabel: GDSLocalisedString = GDSLocalisedString(stringKey: "app_loadingLabel", "app_nameString")
     
     init(analyticsService: OneLoginAnalyticsService,
-         primaryButtonAction: @escaping () -> Void ) {
-        self.analyticsService = analyticsService.addingAdditionalParameters([
+         primaryButtonAction: @escaping () -> Void) {
+        let analyticsService = analyticsService.addingAdditionalParameters([
             OLTaxonomyKey.level2: OLTaxonomyValue.system,
             OLTaxonomyKey.level3: OLTaxonomyValue.undefined
         ])
-        self.primaryButtonViewModel = AnalyticsButtonViewModel(titleKey: "app_unlockButton",
-                                                               analyticsService: analyticsService) {
+        self.didAppear = .action({
+            let screen = ScreenView(id: BiometricEnrolmentAnalyticsScreenID.unlock.rawValue,
+                                    screen: BiometricEnrolmentAnalyticsScreen.unlock,
+                                    titleKey: "one login unlock screen")
+            analyticsService.trackScreen(screen)
+        })
+        
+        self.primaryButtonAction = {
+            let event = ButtonEvent(textKey: "app_unlockButton")
+            analyticsService.logEvent(event)
             primaryButtonAction()
         }
     }
-    
-    func didAppear() {
-        let screen = ScreenView(id: BiometricEnrolmentAnalyticsScreenID.unlock.rawValue,
-                                screen: BiometricEnrolmentAnalyticsScreen.unlock,
-                                titleKey: "one login unlock screen")
-        analyticsService.trackScreen(screen)
-    }
-    
-    func didDismiss() { /* conforming to BaseViewModel */ }
 }
