eeabd3c577b1aeaeb0fc716f53368b78967bbbef
Kotsalidou, Chionia <chionia.kotsalidou@digital.cabinet-office.gov.uk>
2026-07-09 12:43:33 +0100

    test: update Home tests with new function and adjust timeout

--- Tests/UnitTests/TestExtensions/Testing+TestHelpers.swift
+++ Tests/UnitTests/TestExtensions/Testing+TestHelpers.swift
@@ -2,7 +2,7 @@ import Testing
 
 /// Usage:
 /// #expect(await eventually { <what we need to be true after some timne> })
-func eventually(timeout: Duration = .seconds(2),
+func eventually(timeout: Duration = .seconds(5),
                 interval: Duration = .milliseconds(20),
                 _ condition: @escaping () -> Bool) async -> Bool {
     let deadline = ContinuousClock.now + timeout
