f2bf9bd449f3ed59686df42b7d47d4e8c836c694
chionia-gds <chionia.kotsalidou@digital.cabinet-office.gov.uk>
2026-07-09 13:38:56 +0100

    feat: DCMAW-21299 Remove common dependency from Developer menu (#871)
    * feat: remove common dependency from dev menu
    
    * test: convert to Testing
    
    * chore: remove common dependency
    
    * test: update Home tests with new function and adjust timeout

---
+++ Tests/UnitTests/TestExtensions/Testing+TestHelpers.swift
@@ -0,0 +1,14 @@
+import Testing
+
+/// Usage:
+/// #expect(await eventually { <what we need to be true after some timne> })
+func eventually(timeout: Duration = .seconds(5),
+                interval: Duration = .milliseconds(20),
+                _ condition: @escaping () -> Bool) async -> Bool {
+    let deadline = ContinuousClock.now + timeout
+    while ContinuousClock.now < deadline {
+        if condition() { return true }
+        try? await Task.sleep(for: interval)
+    }
+    return false
+}
