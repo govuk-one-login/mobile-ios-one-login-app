457bfd16b759087dca27795e5721a9e96779a96f
shukriahmed-dd <shukri.ahmed@digital.cabinet-office.gov.uk>
2026-07-16 16:17:13 +0100

    chore: update snapshots

--- Tests/SnapshotTests/ErrorScreenSnapshotTests.swift
+++ Tests/SnapshotTests/ErrorScreenSnapshotTests.swift
@@ -2,6 +2,7 @@ import DesignSystem
 import Foundation
 import LocalAuthenticationWrapper
 @testable import OneLogin
+@testable import SnapshotHelpers
 import Testing
 import UIKit
 
@@ -78,20 +79,24 @@ struct ErrorScreenSnapshotTests {
         let root = UINavigationController()
         let sut = AppUnavailableViewModel(analyticsService: analyticsService)
         let vc = GDSScreen(viewModel: sut)
+        
         root.pushViewController(vc, animated: true)
         root.assertSnapshot()
     }
     
     @Test
     func test_dataDeletedWarning() {
+        let root = UINavigationController()
         let sut = DataDeletedWarningViewModel(action: {})
         let vc = GDSScreen(viewModel: sut)
         
-        vc.assertSnapshot()
+        root.pushViewController(vc, animated: true)
+        root.assertSnapshot()
     }
     
     @Test
     func test_genericError() {
+        let root = UINavigationController()
         let sut = GenericErrorViewModel(
             analyticsService: analyticsService,
             errorDescription: PersistentSessionError(.userRemovedLocalAuth).localizedDescription,
@@ -99,22 +104,26 @@ struct ErrorScreenSnapshotTests {
         )
         let vc = GDSScreen(viewModel: sut)
         
-        vc.assertSnapshot()
+        root.pushViewController(vc, animated: true)
+        root.assertSnapshot()
     }
 
     @Test
     func test_networkConnectionError() {
+        let root = UINavigationController()
         let sut = NetworkConnectionErrorViewModel(
             analyticsService: analyticsService,
             action: {}
         )
         let vc = GDSScreen(viewModel: sut)
         
-        vc.assertSnapshot()
+        root.pushViewController(vc, animated: true)
+        root.assertSnapshot()
     }
     
     @Test
     func test_recoverableLoginError() {
+        let root = UINavigationController()
         let sut = RecoverableLoginErrorViewModel(
             analyticsService: analyticsService,
             errorDescription: PersistentSessionError(.userRemovedLocalAuth).localizedDescription,
@@ -122,7 +131,8 @@ struct ErrorScreenSnapshotTests {
         )
         let vc = GDSScreen(viewModel: sut)
         
-        vc.assertSnapshot()
+        root.pushViewController(vc, animated: true)
+        root.assertSnapshot()
     }
     
     @Test
@@ -134,26 +144,31 @@ struct ErrorScreenSnapshotTests {
             action: {}
         )
         let vc = GDSScreen(viewModel: sut)
+        
         root.pushViewController(vc, animated: true)
         root.assertSnapshot()
     }
     
     @Test
     func test_unrecoverableLoginError() {
+        let root = UINavigationController()
         let sut = UnrecoverableLoginErrorViewModel(
             analyticsService: analyticsService,
             errorDescription: PersistentSessionError(.userRemovedLocalAuth).localizedDescription
         )
         let vc = GDSScreen(viewModel: sut)
         
-        vc.assertSnapshot()
+        root.pushViewController(vc, animated: true)
+        root.assertSnapshot()
     }
     
     @Test
     func test_updateAppError() {
+        let root = UINavigationController()
         let sut = UpdateAppViewModel(analyticsService: analyticsService)
         let vc = GDSScreen(viewModel: sut)
         
-        vc.assertSnapshot()
+        root.pushViewController(vc, animated: true)
+        root.assertSnapshot()
     }
 }
