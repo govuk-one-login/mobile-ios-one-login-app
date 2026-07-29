9a4a51eff197570ecaa3e2307a97bfbeab0748a4
Kotsalidou, Chionia <chionia.kotsalidou@digital.cabinet-office.gov.uk>
2026-07-29 13:08:27 +0100

    feat: analytics

--- Sources/Analytics/Tabs/Home/HomeTabAnalytics.swift
+++ Sources/Analytics/Tabs/Home/HomeTabAnalytics.swift
@@ -3,8 +3,10 @@ import Logging
 
 enum HomeAnalyticsScreen: String, OneLoginScreenType {
     case homeScreen
+    case proveIdentityGuidance
 }
 
 enum HomeAnalyticsScreenID: String {
     case homeScreen = "35d856e3-382a-40e5-9758-50784f23e846"
+    case proveIdentityGuidance = "a1d4edc2-2491-4970-a135-90ff0896de23"
 }
