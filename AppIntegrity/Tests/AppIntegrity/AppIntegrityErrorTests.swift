34d3a339db8ba32534f6154cfa8c3408e21743e5
Markos Zoulias Charatzas <Markos.Charatzas@dsit.gov.uk>
2026-08-14 10:47:42 +0100

    feat: DCMAW-21061 update tests based on the changes to GDSUtilities. (#888)
        * added test for kind
        * description by default defaults to case
    
    Additionally, updated test `FirebaseAppIntegrityServiceTests/testDPoPError` that was *falsely* succeeding. Previously, the test used a try catch block; meaning it still passed even if the error was not thrown.
    
    Using #expect to assert that the error is indeed thrown, indicated that the test needed updating.

--- AppIntegrity/Tests/AppIntegrity/AppIntegrityErrorTests.swift
+++ AppIntegrity/Tests/AppIntegrity/AppIntegrityErrorTests.swift
@@ -1,44 +1,78 @@
 @testable import AppIntegrity
+import GDSUtilities
 import Testing
 
 struct AppIntegrityErrorTests {
+    
+    struct Case<Kind: GDSErrorKind>: Sendable {
+        let error: AppIntegrityError<Kind>
+        let debugDescription: String
+        let kind: String
+    }
+
     // swiftlint:disable line_length
     static let allFirebaseAppCheckErrors = [
-        (error: FirebaseAppCheckError(.unknown, reason: "unknown firebase app check service error"), debugDescription: "unknown firebase app check service error"),
-        (error: FirebaseAppCheckError(.network, reason: "network error in firebase app check service"), debugDescription: "network error in firebase app check service"),
-        (error: FirebaseAppCheckError(.invalidConfiguration, reason: "invalid configuration for firebase app check service"), debugDescription: "invalid configuration for firebase app check service"),
-        (error: FirebaseAppCheckError(.keychainAccess, reason: "keychain access error in firebase app check service"), debugDescription: "keychain access error in firebase app check service"),
-        (error: FirebaseAppCheckError(.notSupported, reason: "firebase app check service not supported on this platform"), debugDescription: "firebase app check service not supported on this platform"),
-        (error: FirebaseAppCheckError(.generic, reason: "generic firebase app check service error"), debugDescription: "generic firebase app check service error")
-
+        Case(error: FirebaseAppCheckError(.unknown), debugDescription: "Error Domain=FirebaseAppCheckErrorType Code=1000 \"unknown firebase app check service error\"", kind: "unknown"),
+        Case(error: FirebaseAppCheckError(.network), debugDescription: "Error Domain=FirebaseAppCheckErrorType Code=1001 \"network error in firebase app check service\"", kind: "network"),
+        Case(error: FirebaseAppCheckError(.invalidConfiguration), debugDescription: "Error Domain=FirebaseAppCheckErrorType Code=1002 \"invalid configuration for firebase app check service\"", kind: "invalidConfiguration"),
+        Case(error: FirebaseAppCheckError(.keychainAccess), debugDescription: "Error Domain=FirebaseAppCheckErrorType Code=1003 \"keychain access error in firebase app check service\"", kind: "keychainAccess"),
+        Case(error: FirebaseAppCheckError(.notSupported), debugDescription: "Error Domain=FirebaseAppCheckErrorType Code=1004 \"firebase app check service not supported on this platform\"", kind: "notSupported"),
+        Case(error: FirebaseAppCheckError(.generic), debugDescription: "Error Domain=FirebaseAppCheckErrorType Code=1005 \"generic firebase app check service error\"", kind: "generic")
     ]
     
     static let allClientAssertionErrors = [
-        (error: ClientAssertionError(.invalidPublicKey, reason: "invalid client attestation public key"), debugDescription: "invalid client attestation public key"),
-        (error: ClientAssertionError(.invalidToken, reason: "invalid firebase app check token"), debugDescription: "invalid firebase app check token"),
-        (error: ClientAssertionError(.serverError, reason: "server error"), debugDescription: "server error"),
-        (error: ClientAssertionError(.cantDecodeClientAssertion, reason: "cant decode client attestation"), debugDescription: "cant decode client attestation")
+        Case(error: ClientAssertionError(.invalidPublicKey), debugDescription: "Error Domain=ClientAssertionErrorType Code=1001 \"invalid client attestation public key\"", kind: "invalidPublicKey"),
+        Case(error: ClientAssertionError(.invalidToken), debugDescription: "Error Domain=ClientAssertionErrorType Code=1002 \"invalid firebase app check token\"", kind: "invalidToken"),
+        Case(error: ClientAssertionError(.serverError), debugDescription: "Error Domain=ClientAssertionErrorType Code=2001 \"server error\"", kind: "serverError"),
+        Case(error: ClientAssertionError(.cantDecodeClientAssertion), debugDescription: "Error Domain=ClientAssertionErrorType Code=3001 \"cant decode client attestation\"", kind: "cantDecodeClientAssertion")
     ]
     
     static let allProofOfPossessionErrors = [
-        (error: ProofOfPossessionError(.cantGenerateAttestationPublicKeyJWK, reason: "cant generate attestation public key JWK"), debugDescription: "cant generate attestation public key JWK"),
-        (error: ProofOfPossessionError(.cantGenerateAttestationProofOfPossessionJWT, reason: "cant generate attestation proof of possession JWT"), debugDescription: "cant generate attestation proof of possession JWT"),
-        (error: ProofOfPossessionError(.cantGenerateDemonstratingProofOfPossessionJWT, reason: "can't generate demonstrating public key dictionary JWT"), debugDescription: "can't generate demonstrating public key dictionary JWT")
+        Case(error: ProofOfPossessionError(.cantGenerateAttestationPublicKeyJWK), debugDescription: "Error Domain=ProofOfPossessionErrorType Code=1002 \"cant generate attestation public key JWK\"", kind: "cantGenerateAttestationPublicKeyJWK"),
+        Case(error: ProofOfPossessionError(.cantGenerateAttestationProofOfPossessionJWT), debugDescription: "Error Domain=ProofOfPossessionErrorType Code=1003 \"cant generate attestation proof of possession JWT\"", kind: "cantGenerateAttestationProofOfPossessionJWT"),
+        Case(error: ProofOfPossessionError(.cantGenerateDemonstratingProofOfPossessionJWT), debugDescription: "Error Domain=ProofOfPossessionErrorType Code=1001 \"can't generate demonstrating public key dictionary JWT\"", kind: "cantGenerateDemonstratingProofOfPossessionJWT")
     ]
     // swiftlint:enable line_length
 
-    @Test("assert debugDescription matches reason", arguments: AppIntegrityErrorTests.allFirebaseAppCheckErrors)
-    func test_debugDescription(sut: FirebaseAppCheckError, debugDescription: String) async throws {
-        #expect(sut.debugDescription == debugDescription)
+    @Test("assert debugDescription", arguments: AppIntegrityErrorTests.allFirebaseAppCheckErrors)
+    func test_debugDescription_FirebaseAppCheckError(testCase: Case<FirebaseAppCheckErrorType>) async throws {
+        #expect(testCase.error.debugDescription == testCase.debugDescription)
     }
     
-    @Test("assert debugDescription matches reason", arguments: AppIntegrityErrorTests.allClientAssertionErrors)
-    func test_debugDescription(sut: ClientAssertionError, debugDescription: String) async throws {
-        #expect(sut.debugDescription == debugDescription)
+    @Test("assert debugDescription", arguments: AppIntegrityErrorTests.allClientAssertionErrors)
+    func test_debugDescription_ClientAssertionError(testCase: Case<ClientAssertionErrorType>) async throws {
+        #expect(testCase.error.debugDescription == testCase.debugDescription)
     }
 
-    @Test("assert debugDescription matches reason", arguments: AppIntegrityErrorTests.allProofOfPossessionErrors)
-    func test_debugDescription(sut: ProofOfPossessionError, debugDescription: String) async throws {
-        #expect(sut.debugDescription == debugDescription)
+    @Test("assert debugDescription", arguments: AppIntegrityErrorTests.allProofOfPossessionErrors)
+    func test_debugDescription_ProofOfPossessionError(testCase: Case<ProofOfPossessionErrorType>) async throws {
+        #expect(testCase.error.debugDescription == testCase.debugDescription)
+    }
+
+    /// // swiftlint:disable line_length
+    /// The `kind` found in the `userInfo` **must** hold a unique String identifier that describes the error as reported on analytics
+    /// - Seealso: https://govukverify.atlassian.net/wiki/spaces/DCMAW/pages/3787195450/GOV.UK+One+Login+app+-+Error+handling#App-integrity-check-failures
+    /// // swiftlint:enable line_length
+    @Test("assert kind", arguments: AppIntegrityErrorTests.allFirebaseAppCheckErrors)
+    func test_kind_FirebaseAppCheckError(testCase: Case<FirebaseAppCheckErrorType>) async throws {
+        #expect(testCase.error.errorUserInfo["kind"] as? String == testCase.kind)
+    }
+    
+    /// // swiftlint:disable line_length
+    /// The `kind` found in the `userInfo` **must** hold a unique String identifier that describes the error as reported on analytics
+    /// - Seealso: https://govukverify.atlassian.net/wiki/spaces/DCMAW/pages/3787195450/GOV.UK+One+Login+app+-+Error+handling#App-integrity-check-failures
+    /// // swiftlint:enable line_length
+    @Test("assert kind", arguments: AppIntegrityErrorTests.allClientAssertionErrors)
+    func test_kind_ClientAssertionError(testCase: Case<ClientAssertionErrorType>) async throws {
+        #expect(testCase.error.errorUserInfo["kind"] as? String == testCase.kind)
+    }
+
+    /// // swiftlint:disable line_length
+    /// The `kind` found in the `userInfo` **must** hold a unique String identifier that describes the error as reported on analytics
+    /// - Seealso: https://govukverify.atlassian.net/wiki/spaces/DCMAW/pages/3787195450/GOV.UK+One+Login+app+-+Error+handling#App-integrity-check-failures
+    /// // swiftlint:enable line_length
+    @Test("assert kind", arguments: AppIntegrityErrorTests.allProofOfPossessionErrors)
+    func test_kind_ProofOfPossessionError(testCase: Case<ProofOfPossessionErrorType>) async throws {
+        #expect(testCase.error.errorUserInfo["kind"] as? String == testCase.kind)
     }
 }
--- Tests/UnitTests/Utilities/Session/PersistentSessionErrorTests.swift
+++ Tests/UnitTests/Utilities/Session/PersistentSessionErrorTests.swift
@@ -2,18 +2,35 @@
 import Testing
 
 struct PersistentSessionErrorTests {
+    
+    struct Case: Sendable {
+        let error: PersistentSessionError
+        let debugDescription: String
+        let kind: String
+    }
+
     // swiftlint:disable line_length
     static let allPersistentSessionError = [
-        (error: PersistentSessionError(.noSessionExists, reason: "there was no persistentID token saved in the encrypted store"), debugDescription: "there was no persistentID token saved in the encrypted store"),
-        (error: PersistentSessionError(.userRemovedLocalAuth, reason: "the user has removed all local auth from their device"), debugDescription: "the user has removed all local auth from their device"),
-        (error: PersistentSessionError(.sessionMismatch, reason: "the persistentID was cleared from the encrypted store because a different user logged in"), debugDescription: "the persistentID was cleared from the encrypted store because a different user logged in"),
-        (error: PersistentSessionError(.cannotDeleteData, reason: "there was an error while trying to delete all user data"), debugDescription: "there was an error while trying to delete all user data"),
-        (error: PersistentSessionError(.idTokenNotStored, reason: "there was no idToken found in the secure store"), debugDescription: "there was no idToken found in the secure store")
+        Case(error: PersistentSessionError(.noSessionExists), debugDescription: "Error Domain=PersistentSessionErrorKind Code=1001 \"there was no persistentID token saved in the encrypted store\"", kind: "noSessionExists"),
+        Case(error: PersistentSessionError(.userRemovedLocalAuth), debugDescription: "Error Domain=PersistentSessionErrorKind Code=1002 \"the user has removed all local auth from their device\"", kind: "userRemovedLocalAuth"),
+        Case(error: PersistentSessionError(.sessionMismatch), debugDescription: "Error Domain=PersistentSessionErrorKind Code=1003 \"the persistentID was cleared from the encrypted store because a different user logged in\"", kind: "sessionMismatch"),
+        Case(error: PersistentSessionError(.cannotDeleteData), debugDescription: "Error Domain=PersistentSessionErrorKind Code=1004 \"there was an error while trying to delete all user data\"", kind: "cannotDeleteData"),
+        Case(error: PersistentSessionError(.idTokenNotStored), debugDescription: "Error Domain=PersistentSessionErrorKind Code=1005 \"there was no idToken found in the secure store\"", kind: "idTokenNotStored")
     ]
     // swiftlint:enable line_length
 
-    @Test("assert debugDescription matches reason", arguments: PersistentSessionErrorTests.allPersistentSessionError)
-    func test_debugDescription(sut: PersistentSessionError, debugDescription: String) async throws {
-        #expect(sut.debugDescription == debugDescription)
+    @Test("assert debugDescription", arguments: PersistentSessionErrorTests.allPersistentSessionError)
+    func test_debugDescription(testCase: Case) async throws {
+        #expect(testCase.error.debugDescription == testCase.debugDescription)
     }
+    
+    /// // swiftlint:disable line_length
+    /// The `kind` found in the `userInfo` **must** hold a unique String identifier that describes the error as reported on analytics
+    /// - Seealso: https://govukverify.atlassian.net/wiki/x/OgS84Q
+    /// // swiftlint:enable line_length
+    @Test("assert kind", arguments: PersistentSessionErrorTests.allPersistentSessionError)
+    func test_kind(testCase: Case) async throws {
+        #expect(testCase.error.errorUserInfo["kind"] as? String == testCase.kind)
+    }
+
 }
