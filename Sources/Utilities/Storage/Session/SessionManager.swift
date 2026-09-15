a8dd85ae0c7edbaac3a12d42ca1c0ed5f06fb039
Markos Zoulias Charatzas <markos.charatzas@dsit.gov.uk>
2026-09-15 12:45:54 +0100

    chore: PR comments

--- Sources/Utilities/Storage/Session/SessionManager.swift
+++ Sources/Utilities/Storage/Session/SessionManager.swift
@@ -47,11 +47,10 @@ protocol SessionManager: AnyObject, UserProvider {
     /// - Parameters:
     ///     - idToken: the associated token id for which to perform the refresh token exchange
     ///     - refreshToken: an existing, valid refresh token to be used for the refresh token exchange
-    ///  - throws: ``SecureStoreError(.cantRetrieveKey)`` in case there was an error retrieving the
+    ///  - throws: ``SecureStoreError(.cantRetrieveKey)`` in case there was an error retrieving the public key
     ///  - throws: ``ServerError`` in case of a 400 response
     ///  - throws: ``RefreshTokenExchangeError.noInternet`` in case the network connection was either never
     ///      established, timed out or lost during the refresh token exchange.
-    ///  - throws: a 
     /// - SeeAlso: ``TokenExchangeManaging/getUpdatedTokens(refreshToken:)`` on what a refresh token exchange entails.
     /// - SeeAlso: ``TokenStore`` on how to retrieve the saved tokens
     @MainActor
