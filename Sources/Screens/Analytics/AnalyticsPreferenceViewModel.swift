b14ea3fcb9eba6de3d3ffadc12dc21a778069e12
shukriahmed-dd <shukri.ahmed@digital.cabinet-office.gov.uk>
2026-09-23 12:01:43 +0100

    chore: update DS version

--- Sources/Screens/Analytics/AnalyticsPreferenceViewModel.swift
+++ Sources/Screens/Analytics/AnalyticsPreferenceViewModel.swift
@@ -31,12 +31,13 @@ struct AnalyticsPreferenceViewModel: GDSLeftAlignedViewModel {
                                  alignment: .left,
                                  verticalPadding: .bottom(DesignSystem.Spacing.default)),
                 GDSButtonViewModel(title: GDSLocalisedString(stringKey: "app_privacyNoticeLink", "app_nameString").value,
-                                   style: .secondary.adjusting(alignment: .leading),
+                                   style: .secondaryLeading,
                                    buttonAction: .action {
                                        textButtonAction()
                                    },
                                    accessibilityHint: GDSLocalisedString("app_externalBrowser").value,
-                                   verticalPadding: .bottom(DesignSystem.Spacing.default))
+                                   verticalPadding: .bottom(DesignSystem.Spacing.default),
+                                   horizontalPadding: .horizontal(DesignSystem.Spacing.default))
             ],
             movableFooter: [
                 GDSButtonViewModel(title: GDSLocalisedString(stringKey: "app_shareAnalyticsButton").value,
